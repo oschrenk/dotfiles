5fe09aefd124d785ec70e2e3588be46d9b9172c8
Oliver Schrenk <oliver.schrenk@gmail.com>
2014-08-18 12:59:10 +0200

    Use lokaltog/powerline

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,11 @@ if status --is-login
     . ~/.config/fish/env.fish
 end
 
-
 # local configurations
 if test -r ~/.config/fish/local.fish
   source ~/.config/fish/local.fish
 end
+
+# Powerline
+set fish_function_path $fish_function_path "/usr/local/lib/python2.7/site-packages/powerline/bindings/fish"
+powerline-setup
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,3 +1,2 @@
 function fish_prompt
-  ~/.powerline-shell.py $status --cwd-max-depth 3 --shell bare ^/dev/null
 end
