eaf40e43e4c9f9597282fc06cc8901ccc7abfc68
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-03-12 17:38:24 +0100

    fish: Use bobthefish theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,3 @@ if test -r ~/.config/fish/local.fish
   source ~/.config/fish/local.fish
 end
 
-# Powerline
-set fish_function_path $fish_function_path "/usr/local/lib/python2.7/site-packages/powerline/bindings/fish"
-powerline-setup
--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,3 +1,7 @@
-function fish_greeting
-  fortune
+function fish_greeting -d "what's up, fish?"
+  set_color $fish_color_autosuggestion[1]
+  uname -npsr
+  uptime
+  set_color normal
 end
+
