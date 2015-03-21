6c1b68da57f3a42936df73d47aa42dc8faa1315e
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-03-22 00:24:05 +0100

    tilde.fish: implementing deja in fish

---
+++ .config/fish/functions/tilde.fish
@@ -0,0 +1,70 @@
+function __tilde_help
+  echo "Unrecognized command."
+end
+
+function __tilde_ignore_patterns
+  set -l repo $argv[1]
+  set -l ignore_file $repo/.tildeignore
+
+  if test -e $ignore_file
+    paste -s -d" " (sed '/^[[:space:]]*$/d' $repo/.tildeignore | awk '{print " ! -name \""$0"\""}' | psub)
+  else
+    ""
+  end
+end
+
+function __tilde_linkable_files
+  set -l repo $argv[1]
+  set -l ignore_patterns (__tilde_ignore_patterns $repo)
+  set -l search_patterns "\\( -name '*'$ignore_patterns \\)"
+
+  eval find $repo -depth 1 "$search_patterns" | grep -v ".tildeignore"
+end
+
+function __tilde_link
+  set -l symlink_dir $HOME
+  set -l tilde_home $HOME/.tilde
+  set -l tilde_repo $HOME/.tilde/$argv[1]
+
+  if not test -d $tilde_repo
+    echo "No $tilde_repo directory found. Exiting."
+    return
+  end
+
+  echo "Syncing $tilde_repo with $symlink_dir"
+
+  for file in (__tilde_linkable_files $tilde_repo)
+    set -l base_name (basename $file)
+    set -l source_path $file
+    set -l target_path $symlink_dir/$base_name
+
+    # TODO test if trying to symlink .tilde
+    if test -L $target_path
+      echo "Skipped $source_path: Already exists as symbolic link"
+    else if test -f $target_path
+      echo "Skipped $source_path: Already exists as regular file"
+    else
+      echo "Symlinking $target_path to $source_path"
+      ln -s $source_path $target_path
+    end
+  end
+end
+
+function tilde --description  "deja in fish"
+  set -l tilde_home $HOME/.tilde
+  if not test -d $tilde_home
+    echo "No $tilde_home directory found. Exiting."
+    return
+  end
+
+  if test (count $argv) -ne 2
+    __tilde_help
+    return
+  end
+
+  set -l subcommand $argv[1]
+
+  if [ "$subcommand" = "link" ]
+    __tilde_link $argv[2]
+  end
+end
