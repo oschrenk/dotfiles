eff8b8878309f6c357fd88fc46265c6ccd4574ad
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-03-24 00:40:50 +0100

    hammerspoon: Show "Config reloaded" only on reload

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -149,8 +149,8 @@ hs.hotkey.bind(hyper, '0', function() pom_disable() end)
 function reload_config(files)
   wifiWatcher:stop()
   hs.reload()
+  hs.alert.show("Config reloaded")
 end
 
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
-hs.alert.show("Config reloaded")
 
