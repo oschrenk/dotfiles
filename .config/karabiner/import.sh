24a93e8c4e4dd864b253ffc484effdf90e42cc41
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-03-30 18:34:16 +0200

    karabiner: Disable ([{}]) mappings for modifiers

--- .config/karabiner/import.sh
+++ .config/karabiner/import.sh
@@ -2,21 +2,21 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set remap.hjkl_arrow 1
+$cli set remap.fkeys_to_consumer_f7 1
 /bin/echo -n .
 $cli set remap.org.openhort.german_umlaute_on_en_US 1
 /bin/echo -n .
-$cli set remap.fkeys_to_consumer_f7 1
+$cli set repeat.initial_wait 250
 /bin/echo -n .
 $cli set repeat.wait 25
 /bin/echo -n .
 $cli set remap.escape2backquote 1
 /bin/echo -n .
+$cli set remap.euro_in_us 1
+/bin/echo -n .
 $cli set general.disable_internal_keyboard_if_external_keyboard_exsits 1
 /bin/echo -n .
 $cli set user.iterm.cmdq 1
-/bin/echo -n .
-$cli set remap.circumflex_in_us 1
 /bin/echo -n .
 $cli set remap.fkeys_to_consumer_f1 1
 /bin/echo -n .
@@ -26,10 +26,6 @@ $cli set remap.controlL2controlL_escape 1
 /bin/echo -n .
 $cli set private.correct_shift_keys 1
 /bin/echo -n .
-$cli set remap.cmdcurlies 1
-/bin/echo -n .
-$cli set remap.programmer.shifts_parens 1
-/bin/echo -n .
 $cli set remap.fn_fkey_f1_consumer 1
 /bin/echo -n .
 $cli set user.iterm.cmdw 1
@@ -38,12 +34,10 @@ $cli set remap.shiftEscape2tilde 1
 /bin/echo -n .
 $cli set space_cadet.left_control_to_hyper 1
 /bin/echo -n .
-$cli set remap.altbrackets 1
+$cli set parameter.keyoverlaidmodifier_timeout 500
 /bin/echo -n .
 $cli set user.hitlist.cmdw 1
 /bin/echo -n .
-$cli set parameter.keyoverlaidmodifier_timeout 500
-/bin/echo -n .
-$cli set repeat.initial_wait 250
+$cli set remap.hjkl_arrow 1
 /bin/echo -n .
 /bin/echo
