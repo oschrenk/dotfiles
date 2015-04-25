b09da0c8abb006bfb2aad44ec192527e632b1fca
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-04-25 14:40:13 +0200

    tilde: Update

--- .config/fish/functions/tilde.fish
+++ .config/fish/functions/tilde.fish
@@ -1,7 +1,7 @@
 function __tilde_help
   echo "Unrecognized command."
   echo ""
-  echo "Usage: tilde COMMAND [OPTIONS]"
+  echo "Usage: tilde <COMMAND> [OPTIONS]"
   echo ""
   echo "Commands:"
   echo ""
@@ -11,7 +11,7 @@ function __tilde_help
 end
 
 function __tilde_ignore_patterns
-  set -l tilde_dir $argv[1]i
+  set -l tilde_dir $argv[1]
   # TODO it might be a good idea to merge .tildeignore files from various levels
   # At the moment it only really reads ignores for current directory. Perhaps
   # we should merge with some sort of global ignores and repo ignores
@@ -31,79 +31,6 @@ function __tilde_linkable_files
   set -l search_patterns "-depth 1 \\( -name '*' $default_ignore_patterns $dynamic_ignore_patterns \\)"
 
   eval find $dir "$search_patterns"
-end
-
-
-# When are files symlinked?
-# 1. dotfiles_home/file_path points to repo_dir/file_path
-# 2. parent directories is symlinked and path token is same
-function __is_linked
-  echo $argv | read -l dotfiles_home tilde_repo_dir file_path_suffix
-  if $file_path_suffix = "."
-    return 0
-  end
-
-  set -l symlink_path $dotfiles_home/$file_path_suffix
-  set -l repo_path $repo_dir/$file_path_suffix
-
-  # symlink points to repo
-  if readlink $symlink_path = $repo_path
-    echo "symlinked"
-  else if test -e $symlink_path
-    __is_linked $dotfiles_home $repo_dir (dirname $file_path_suffix)
-  end
-end
-
-function __tilde_meta_tags
-  echo $argv | read -l dotfiles_home tilde_repo_dir file_path_suffix
-
-  set -l symlink_path $dotfiles_home/$file_path_suffix
-  set -l tilde_path $tilde_repo_dir/$file_path_suffix
-
-  set -l meta_tags ""
-
-  if test -d $tilde_path
-    # TODO what are the implications of set without `-l`
-    set meta_tags "dir, $meta_tags"
-  end
-
-  # TODO it seems that -f also sees symlinks
-  # TODO make sure that if symlink to tilde_path its fine
-  if test -e $symlink_path -a not test -L $symlink_path
-    set meta_tags "conflicts, $meta_tags"
-  end
-
-  if not __is_linked $dotfiles_home $tilde_repo_dir $file_path_suffix
-    set meta_tags "unlinked, $meta_tags"
-  end
-
-  echo "[ $meta_tags ]"
-end
-
-function __tilde_ls
-  echo $argv | read -l dotfiles_home tilde_home tilde_rel_dir
-  set -l tilde_abs_dir $tilde_home/$tilde_rel_dir
-
-  if not test -d $tilde_abs_dir
-    echo "Error: directory $tilde_abs_dir not found"
-    return
-  end
-
-  set -l repo_rel_dir (echo $tilde_rel_dir | awk -F "/" '{$1="";print $0}' OFS='/' | cut -c 2-)
-  set -l dotfiles_home $dotfiles_home/$repo_rel_dir
-
-  echo "dotfiles_home $dotfiles_home"
-  echo "tilde_home    $tilde_home"
-  echo "tilde_rel_dir $tilde_rel_dir"
-  echo "repo_rel_dir  $repo_rel_dir"
-  echo "dotfiles_home $dotfiles_home"
-
-  for tilde_path in (__tilde_linkable_files $tilde_abs_dir)
-    set -l name (basename $tilde_path)
-    set -l symlink_path $dotfiles_home$name
-
-    #echo $name (__tilde_meta_tags $symlink_path $tilde_path)
-  end
 end
 
 function __tilde_clone
