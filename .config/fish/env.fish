20fe3923f0d48633cce2a3b24e6cd5b83f27aa1f
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-06-02 23:32:41 +0200

    fish: Fix gruvbox color palette

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -38,3 +38,6 @@ set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
 set -x SCALA_HOME /usr/local/opt/scala/
 set -x SPARK_HOME $HOME/Frameworks/spark
 
+# Color scheme
+source gruvbox_256palette_osx.fish
+
