fd437f1f8acd7344292b29b1ed2bf1c4178f4c60
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-07-21 00:15:44 +0200

    fish: Setup default PATH
    so that fish can find its commands during setup phase

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,10 +1,7 @@
 # This file is sourced on login shells only
 # (responsible for setting up initial environment)
 
-set -gx PATH /bin
-append-to-path /sbin
-append-to-path /usr/bin
-append-to-path /usr/sbin
+set -gx PATH "/bin:/sbin/:/usr/bin:/usr/sbin"
 
 # put homebrew bin before system bin
 prepend-to-path /usr/local/sbin
