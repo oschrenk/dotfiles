cbd59fb0b56a50e96832c29a2f945f24a4fd8c82
Oliver Schrenk <oliver.schrenk@gmail.com>
2016-05-19 14:39:30 +0200

    karabiner: Enable vim mode only in mail selection
    vim mode is broken when adding calendar items in Mail

--- .config/karabiner/import.sh
+++ .config/karabiner/import.sh
@@ -2,50 +2,52 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set general.disable_internal_keyboard_if_external_keyboard_exsits 1
+/bin/echo -n .
+$cli set parameter.keyoverlaidmodifier_timeout 500
+/bin/echo -n .
+$cli set private.correct_shift_keys 1
+/bin/echo -n .
+$cli set private.hjkl_in_mail 1
+/bin/echo -n .
+$cli set remap.circumflex_in_us 1
+/bin/echo -n .
+$cli set remap.controlL2controlL_escape 1
+/bin/echo -n .
+$cli set remap.escape2backquote 1
+/bin/echo -n .
+$cli set remap.euro_in_us 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f1 1
+/bin/echo -n .
 $cli set remap.fkeys_to_consumer_f7 1
 /bin/echo -n .
+$cli set remap.fn_fkey_f1_consumer 1
+/bin/echo -n .
+$cli set remap.hjkl_arrow 1
+/bin/echo -n .
 $cli set remap.org.openhort.german_umlaute_on_en_US 1
+/bin/echo -n .
+$cli set remap.shiftEscape2tilde 1
 /bin/echo -n .
 $cli set repeat.initial_wait 250
 /bin/echo -n .
 $cli set repeat.wait 25
 /bin/echo -n .
-$cli set remap.escape2backquote 1
-/bin/echo -n .
-$cli set user.iterm.cmdaltsemicolon 1
-/bin/echo -n .
-$cli set remap.euro_in_us 1
-/bin/echo -n .
-$cli set user.iterm.cmdq 1
-/bin/echo -n .
-$cli set remap.circumflex_in_us 1
-/bin/echo -n .
-$cli set general.disable_internal_keyboard_if_external_keyboard_exsits 1
-/bin/echo -n .
-$cli set remap.fkeys_to_consumer_f1 1
-/bin/echo -n .
-$cli set user.hitlist.cmdq 1
-/bin/echo -n .
-$cli set remap.controlL2controlL_escape 1
-/bin/echo -n .
-$cli set user.dayone.cmdw 1
-/bin/echo -n .
-$cli set private.correct_shift_keys 1
-/bin/echo -n .
-$cli set remap.fn_fkey_f1_consumer 1
-/bin/echo -n .
-$cli set user.iterm.cmdw 1
-/bin/echo -n .
-$cli set remap.shiftEscape2tilde 1
-/bin/echo -n .
 $cli set space_cadet.left_control_to_hyper 1
 /bin/echo -n .
 $cli set user.dayone.cmdq 1
 /bin/echo -n .
-$cli set parameter.keyoverlaidmodifier_timeout 500
+$cli set user.dayone.cmdw 1
+/bin/echo -n .
+$cli set user.hitlist.cmdq 1
 /bin/echo -n .
 $cli set user.hitlist.cmdw 1
 /bin/echo -n .
-$cli set remap.hjkl_arrow 1
+$cli set user.iterm.cmdaltsemicolon 1
+/bin/echo -n .
+$cli set user.iterm.cmdq 1
+/bin/echo -n .
+$cli set user.iterm.cmdw 1
 /bin/echo -n .
 /bin/echo
