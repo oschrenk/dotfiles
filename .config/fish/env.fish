0c865f19273121c88b26e18eb780881b15f69184
Oliver Schrenk <oliver.schrenk@gmail.com>
2016-06-03 11:11:10 +0200

    fish/evn: Remove default PATH
    Confuses fish 2.3

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,9 +1,6 @@
 # This file is sourced on login shells only
 # (responsible for setting up initial environment)
 
-# set default PATH
-set -gx PATH /bin:/sbin/:/usr/bin:/usr/sbin
-
 # put homebrew bin before system bin
 prepend-to-path /usr/local/sbin
 prepend-to-path /usr/local/bin
