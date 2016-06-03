eb2a3db6edb8dbc3856f5a895cd91e634f420c52
Oliver Schrenk <oliver.schrenk@gmail.com>
2016-06-03 11:25:34 +0200

    config: Remove default fish prompt before custom
    fish 2.3 brought their own prompt back when enabling vi mode, had to
    disable it to only show my own.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,10 +9,17 @@ if test -r ~/.config/fish/local.fish
   source ~/.config/fish/local.fish
 end
 
+# Disable default theme
+function fish_mode_prompt
+end function
+
 # vi mode, start in insert mode
-set fish_bind_mode insert
+fish_vi_key_bindings
+set -g __fish_vi_mode 1
+
+# enable vi_mode in bob the fish theme
+set -g theme_display_vi yes
 
 # Color scheme
 source $HOME/.config/fish/gruvbox_256palette_osx.fish
 
-set -g theme_display_vi yes
