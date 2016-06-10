e1aaf67978dba68bf6a88e6cc48a976a4820b33c
Oliver Schrenk <oliver.schrenk@gmail.com>
2016-06-10 21:51:56 +0200

    fish/today: Support long format with st,nd,rd,th

--- .config/fish/functions/today.fish
+++ .config/fish/functions/today.fish
@@ -1,10 +1,35 @@
-function today --description "Show todays date is ISO-8601 format"
+function today --description "Show todays date"
   echo $argv | read -l arg
 
   switch $arg
+    case "--long"
+      set -l suffix (__daySuffix (date +"%d"))
+      date +"%A, %d$suffix %B %Y"
     case "--dash"
       date +"%Y-%m-%d"
     case ""
       date +"%Y%m%d"
   end
 end
+
+function __daySuffix
+  switch $argv[1]
+    case "1"
+      "st"
+    case "2"
+      "nd"
+    case "3"
+      "rd"
+    case "21"
+      "st"
+    case "22"
+      "nd"
+    case "23"
+      "rd"
+    case "31"
+      "st"
+    case "*"
+      echo "th"
+    end
+end
+
