3969d404ea356c5264e8483e1f5d505b08f45a59
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-04-22 07:59:36 +0200

    fish: Show fzf project results 40% height, reverse

--- .config/fish/functions/fzf_projects.fish
+++ .config/fish/functions/fzf_projects.fish
@@ -1,6 +1,10 @@
 function fzf_projects --description "Browse git projects using fzf"
   set -l base_dir $HOME/Projects
   set -l length (math (string length $base_dir) + 2)
+
+  set -q FZF_TMUX_HEIGHT; or set FZF_TMUX_HEIGHT 40%
+  set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT --reverse $FZF_DEFAULT_OPTS"
+
   set -q FZF_PROJECT_COMMAND; or set -l FZF_PROJECT_COMMAND "
   find $base_dir -type d -name ".git" -maxdepth 4 | rev | cut -c6- | rev | cut -c$length-"
   # Fish hangs if the command before pipe redirects (2> /dev/null)
