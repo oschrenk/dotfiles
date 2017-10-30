8beff1802eaecfbf282453a2ca81c6eaae69faf7
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-10-30 21:38:42 +0100

    fish, nvim: Rewrite using Ctrl-T, allow for Ctrl-c

--- .config/fish/functions/fzf_nvim.fish
+++ .config/fish/functions/fzf_nvim.fish
@@ -1,3 +1,8 @@
 function fzf_nvim --description "Open file in nvim"
-  nvim (fzf)
+  eval "$FZF_CTRL_T_COMMAND" | fzf --ansi --no-sort --reverse | while read -l r; set result $result $r; end
+  if [ -z $result ]
+    commandline -f repaint
+else
+    nvim $result
+  end
 end
