9e6eaaf5d7f12c001c6087d687fa87ca064db281
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-11-21 10:37:11 +0100

    hammerspoon: Remove afplay

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -516,7 +516,6 @@ hs.hotkey.bind(hyper, 'f', function() hs.window.focusedWindow():toggleFullScreen
 hs.hotkey.bind(hyper, "q", send_window_to_prev_monitor)
 hs.hotkey.bind(hyper, "e", send_window_to_next_monitor)
 
-hs.hotkey.bind(hyper, "p", play)
 hs.hotkey.bind(hyper, "h", connectHeadphones)
 
 hs.hotkey.bind(hyper, "c", closeNotifications)
