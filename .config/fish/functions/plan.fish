89c1d583d53574feae6ce2706a39e159ebb549a6
Oliver Schrenk <oliver.schrenk@gmail.com>
2018-01-03 21:53:03 +0100

    fish, plan: Invoke for other days

--- .config/fish/functions/plan.fish
+++ .config/fish/functions/plan.fish
@@ -1,4 +1,9 @@
-function plan  --description "Create and show tomorrow's journal entry in $EDITOR"
-  set -l date (next 1)
-  journal "$date"
+function plan --description "Create and show tomorrow's journal entry in $EDITOR"
+  if test (count $argv) -eq 1
+    echo $argv | read -l day
+    journal (next $day)
+  else
+    set -l date (next 1)
+    journal "$date"
+  end
 end
