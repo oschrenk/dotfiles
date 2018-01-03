14521ad1ae74ba7aebef44813b9ad4193720a5e9
Oliver Schrenk <oliver.schrenk@gmail.com>
2018-01-03 01:46:43 +0100

    fish, journal: Simplify plan, Re-use journal

--- .config/fish/functions/journal.fish
+++ .config/fish/functions/journal.fish
@@ -1,8 +1,19 @@
-function journal --description "Create and show todays journal entry in $EDITOR"
-  set -l logfile $JOURNAL_DIR/(today --iso-short).md
+function journal --description "Create and show a journal entry in $EDITOR"
+  function __date
+    echo $argv | read -l date
+    if test -z "$date"
+      day
+    else
+      day "$date"
+    end
+  end
+
+  set -l date (__date $argv[1])
+
+  set -l logfile $JOURNAL_DIR/(day --iso-short $date).md
   # Create file if it doesn't exist
   if not test -f $logfile
-    set -l date (today --pretty)
+    set -l date (day --pretty $date)
     echo "# $date" >> $logfile
     # use template
     echo "" >> $logfile
--- .config/fish/functions/plan.fish
+++ .config/fish/functions/plan.fish
@@ -1,15 +1,4 @@
 function plan  --description "Create and show tomorrow's journal entry in $EDITOR"
-
-  set -l logfile $JOURNAL_DIR/(tomorrow --iso-short).md
-  # Create file if it doesn't exist
-  if not test -f $logfile
-    set -l date (tomorrow --pretty)
-    echo "# $date" >> $logfile
-    # use template
-    echo "" >> $logfile
-    cat $HOME/.config/journal/template.md >> $logfile
-  end
-
-  eval "$EDITOR -c 'Goyo' $logfile"
-
+  set -l date (next 1)
+  journal "$date"
 end
