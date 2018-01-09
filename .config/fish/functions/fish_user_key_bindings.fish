72a7187855f7b206982fc2d4035148c57a41788e
Oliver Schrenk <oliver.schrenk@gmail.com>
2018-01-09 09:19:49 +0100

    fish, key-bindings: No more vim mode

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 fzf_key_bindings
 
-bind -M insert \ce edit_command
-bind -M insert \cp fzf_projects
-bind -M insert \co fzf_nvim
-bind -M insert \cg fzf_commits
+bind \ce edit_command
+bind \cp fzf_projects
+bind \co fzf_nvim
+bind \cg fzf_commits
