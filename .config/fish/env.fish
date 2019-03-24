72de8f3a9e863323506e465cd22a51221090c402
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-03-24 16:11:22 +0100

    fish,fzf: Try fd and preview for opening files

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -47,7 +47,7 @@ set -x HOMEBREW_NO_ANALYTICS 1
 # --follow: Follow symlinks
 # --glob: Additional conditions for search (in this case ignore everything in the .git/ folder)
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!{.git,node_modules,target}/*"'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_CTRL_T_COMMAND='fd --type f --type d --hidden --follow --exclude .git'
 
 # Applications
 
