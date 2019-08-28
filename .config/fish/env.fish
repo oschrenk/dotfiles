78fab14d3a5158c9fdc0e513838f8951975a3fda
Oliver Schrenk <oliver.schrenk@gmail.com>
2019-08-28 15:41:10 +0200

    fish,env: Add comment about dependencies

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -47,5 +47,7 @@ set -x HOMEBREW_NO_ANALYTICS 1
 # --follow: Follow symlinks
 # --glob: Additional conditions for search (in this case ignore everything in the .git/ folder)
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!{.git,node_modules,target}/*"'
+
+# relies on `brew install fd`
 export FZF_CTRL_T_COMMAND='fd --type f --type d --hidden --follow --exclude .git'
 
