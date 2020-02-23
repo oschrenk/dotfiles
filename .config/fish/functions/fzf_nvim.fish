ef3e105eabafd5d71df5fe12962f28051682aaa3
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-02-23 21:08:06 +0100

    fish, fzf: Use preview.sh,preview.rb is deprecated

--- .config/fish/functions/fzf_nvim.fish
+++ .config/fish/functions/fzf_nvim.fish
@@ -1,5 +1,5 @@
 function fzf_nvim --description "Open file in nvim"
-  eval "$FZF_CTRL_T_COMMAND" | fzf --ansi --no-sort --reverse --preview '~/.config/nvim/plugged/fzf.vim/bin/preview.rb {} | head -200' | while read -l r; set result $result $r; end
+  eval "$FZF_CTRL_T_COMMAND" | fzf --ansi --no-sort --reverse --preview '~/.config/nvim/plugged/fzf.vim/bin/preview.sh {} | head -200' | while read -l r; set result $result $r; end
   if [ -z $result ]
     commandline -f repaint
 else
