5ea6ca49bccefe70cc99e868e076096e3f50cf04
Oliver Schrenk <oliver.schrenk@gmail.com>
2021-03-18 22:44:38 +0100

    fish: Much greeting. Such wow.

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,10 +1,3 @@
 function fish_greeting -d "What's up, fish?"
-    set_color $fish_color_autosuggestion
-    uname -nmsr
-
-    # TODO: `command -q -s` only works on fish 2.5+, so hold off on that for now
-    command -s uptime >/dev/null
-    and command uptime
-
-    set_color normal
+  doge
 end
