db81404dbeeca8e231b43ad953a082df372e6604
Oliver Schrenk <oliver.schrenk@gmail.com>
2021-03-20 16:42:35 +0100

    fish: Add ctrl+w to fzf inject jira tickets

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,6 @@
 fzf_key_bindings
 
+bind \cw fzf_jira
 bind \cp fzf_projects
 bind \co fzf_nvim
 bind \cg fzf_commits
---
+++ .config/fish/functions/fzf_jira.fish
@@ -0,0 +1,25 @@
+  function fzf_jira -d "List tickets"
+    set -l commandline (__fzf_parse_commandline)
+    set -l fzf_query $commandline[2]
+
+    set -l FZF_JIRA_COMMAND "command jira sprint"
+
+    test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 40%
+    begin
+      set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT --reverse --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS"
+      eval "$FZF_JIRA_COMMAND | "(__fzfcmd)' -m --query "'$fzf_query'"' | while read -l r; set result $result $r; end
+    end
+    if [ -z "$result" ]
+      commandline -f repaint
+      return
+    else
+      # Remove last token from commandline.
+      commandline -t ""
+    end
+    echo $result
+    for i in $result
+      commandline -it -- $prefix
+      commandline -it -- (string split -f1 ':' $i)
+    end
+    commandline -f repaint
+  end
