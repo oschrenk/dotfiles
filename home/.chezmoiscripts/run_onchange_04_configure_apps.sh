c9080f715b63344f063c58268ebfdc560359fb13
Oliver Schrenk <oliver.schrenk@gmail.com>
2022-12-25 09:30:17 +0100

    macos: Apps: echo settings

--- home/.chezmoiscripts/run_onchange_04_configure_apps.sh
+++ home/.chezmoiscripts/run_onchange_04_configure_apps.sh
@@ -6,33 +6,33 @@
 # CHROME
 #######################################
 
-# Use the system-native print preview dialog
+echo "Chrome: Use the system-native print preview dialog"
 defaults write com.google.Chrome DisablePrintPreview -bool true
 
 #######################################
 # HAMMERSPOON
 #######################################
 
-# Move Hammerspoon config file to XDG_CONFIG_HOME
+echo "Hammerspoon: Move config file to XDG_CONFIG_HOME"
 defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
 
 #######################################
 # MAIL
 #######################################
 
-# Forces messages in Mail to be displayed as plain text instead of formatted (0 to reverse)
+echo "Mail: Force messages to be displayed as plain text instead of formatted (0 to reverse)"
 defaults write com.apple.mail PreferPlainText -bool true
 
-# don't add invitations to iCal automatically
+echo "Mail: Don't add invitations to iCal automatically"
 defaults write com.apple.mail AddInvitationsToICalAutomatically -bool false
 
-# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
+echo "Mail: Copy email addresses as foo@example.com instead of Foo Bar <foo@example.com> in Mail.app"
 defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 
-# Disable inline attachments (just show the icons)
+echo "Mail: Disable inline attachments (just show the icons)"
 defaults write com.apple.mail DisableInlineAttachmentViewing -bool false
 
-# Disable send and reply animations in Mail.app
+echo "Mail: Disable send and reply animations in Mail.app"
 defaults write com.apple.Mail DisableReplyAnimations -bool true
 defaults write com.apple.Mail DisableSendAnimations -bool true
 
@@ -40,26 +40,26 @@ defaults write com.apple.Mail DisableSendAnimations -bool true
 # SAFARI
 #######################################
 
-# Privacy: Don’t send search queries to Apple
+echo "Safari: Privacy: Don’t send search queries to Apple"
 defaults write com.apple.Safari UniversalSearchEnabled -bool false
 defaults write com.apple.Safari SuppressSearchSuggestions -bool true
 
-# Disable Java
+ echo "Safari: Disable Java"
 defaults write com.apple.Safari WebKitJavaEnabled -bool false
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled -bool false
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabledForLocalFiles -bool false
 
-# Block pop-up windows
+echo "Safari: Block pop-up windows"
 defaults write com.apple.Safari WebKitJavaScriptCanOpenWindowsAutomatically -bool false
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool false
 
-# Enable “Do Not Track”
+echo "Safari: Enable “Do Not Track"
 defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
 
-# Update extensions automatically
+echo "Safari: Update extensions automatically"
 defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
 
-# Warn about fraudulent websites
+echo "Safari: Warn about fraudulent websites"
 defaults write com.apple.Safari WarnAboutFraudulentWebsites -bool true
 
 ###########################################################
