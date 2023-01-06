9159466630e54efc277d01368dfbee9fd7efb876
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-01-06 22:10:21 +0100

    nvim: Set leaders before anything else

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -1,6 +1,11 @@
+-- Leader/local leader
+vim.g.mapleader = [[ ]]
+vim.g.maplocalleader = [[,]]
+
 require('impatient')
 require('options')
 require('actions')
 require('fern')
 require('keymaps')
 require('plugins')
+
