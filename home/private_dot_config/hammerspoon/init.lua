56421b94a5356186d361698e0c582a3cf92a50e8
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-05-19 11:46:18 +0200

    hammerspoon: Warn if private browsing

--- home/private_dot_config/hammerspoon/init.lua
+++ home/private_dot_config/hammerspoon/init.lua
@@ -85,6 +85,65 @@ hs.hotkey.bind(hyper, "v", toggleWifi)
 
 hs.hotkey.bind(hyper, 'm', mute)
 
+---
+
+
+function handleGlobalEvent(appName, eventType, appObject)
+  if (eventType == hs.application.watcher.activated) then
+    print("activated " .. appName)
+  end
+
+  if (eventType == hs.application.watcher.launched) then
+    print("launched " .. appName)
+  end
+end
+
+function handleAppEvent(element, event, watcher, info)
+  local appName = element:application():title()
+  local windowTitle = element:title()
+
+  if event == hs.uielement.watcher.windowCreated then
+    print("created")
+    if appName:find("Google Chrome") then
+      if windowTitle:find("(Private)", 1, true) then
+        if hs.application.find('OpenVPN Connect') then
+          notify("Created private window created while on VPN")
+        end
+      end
+    end
+  elseif event == hs.uielement.watcher.focusedWindowChanged then
+    if appName:find("Google Chrome") then
+      if windowTitle:find("(Private)", 1, true) then
+        if hs.application.find('OpenVPN Connect') then
+          notify("Switched to private window created while on VPN")
+        end
+      end
+    end
+
+  end
+end
+
+function watchApp(app, initializing)
+  if appWatchers[app:pid()] then return end
+
+  local watcher = app:newWatcher(handleAppEvent)
+  appWatchers[app:pid()] = {
+    watcher = watcher
+  }
+
+  watcher:start({ hs.uielement.watcher.focusedWindowChanged })
+end
+
+function attachExistingApps()
+  local apps = hs.application.runningApplications()
+  apps = hs.fnutils.filter(apps, function(app) return app:title() ~= "Hammerspoon" end)
+  hs.fnutils.each(apps, function(app) watchApp(app, true) end)
+end
+
+appWatchers = {}
+globalWatcher = hs.application.watcher.new(handleGlobalEvent):start()
+attachExistingApps()
+
 ------------------------
 -- Reload
 ------------------------
