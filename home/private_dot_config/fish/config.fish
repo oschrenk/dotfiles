8b0bf4b4ec3c8a34115f3f497bfec9a49f041aec
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-07-29 20:30:38 +0200

    fish: Configure bobthefish

--- home/private_dot_config/fish/config.fish
+++ home/private_dot_config/fish/config.fish
@@ -15,7 +15,23 @@ end
 # Enable direnv
 eval (direnv hook fish)
 
-set -g theme_newline_cursor clean
-set -g theme_display_k8s_context yes
-set -g theme_display_k8s_namespace yes
+set -g theme_display_cmd_duration no
+set -g theme_display_date no
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_dirty_verbose yes
+set -g theme_display_git_stashed_verbose yes
+set -g theme_display_git_untracked no
+set -g theme_display_virtualenv no
 set -g theme_nerd_fonts yes
+set -g theme_newline_cursor clean
+
+# only show k8s context when in $HOME/Projects
+function react_to_pwd --on-variable PWD
+  if string match -e -- "$HOME/Projects" "$PWD"
+    set -g theme_display_k8s_context yes
+    set -g theme_display_k8s_namespace yes
+  else
+    set -g theme_display_k8s_context no
+    set -g theme_display_k8s_namespace no
+  end
+end
