026489e7c0a6a62affa43f9e141d465155dcfabc
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-08-12 00:09:11 +0200

    tmux: Make github PR status session dependent

--- home/private_dot_config/tmux/executable_github_prs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-gh pr -R vandebron/Vandebron list --search "is:open is:pr review-requested:oschrenk archived:false" --json number | jq -r '.[] | .number = "#\(.number)" | .number' | head -3 | awk -v d=" " '{s=(NR==1?s:s d)$0}END{print s}'
