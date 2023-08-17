7982e0d974e59fa4c8c0a7f87add0174d0c5a30f
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-08-17 16:54:13 +0200

    fish, sbt: Fix argv wrapping

--- home/private_dot_config/fish/functions/sbt.fish
+++ home/private_dot_config/fish/functions/sbt.fish
@@ -1,6 +1,6 @@
 function sbt --description "run sbt only with build.sbt file present in pwd"
   if test -e (pwd)/build.sbt
-    command sbt "$argv"
+    command sbt $argv
   else
     echo "Missing build.sbt file" 1>&2
     return 1
