39da708c0a12211add966d271b40d478476f6069
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-08-17 14:41:53 +0200

    fish: Only run sbt when build.sbt available

---
+++ home/private_dot_config/fish/functions/sbt.fish
@@ -0,0 +1,8 @@
+function sbt --description "run sbt only with build.sbt file present in pwd"
+  if test -e (pwd)/build.sbt
+    command sbt "$argv"
+  else
+    echo "Missing build.sbt file" 1>&2
+    return 1
+  end
+end
