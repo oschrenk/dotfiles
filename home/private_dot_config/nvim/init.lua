f96d53473ff2847f1453cbb1289567f5abc77dd9
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-08-19 02:24:26 +0200

    nvim: Update broke loading order

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -2,7 +2,39 @@
 vim.g.mapleader = [[ ]]
 vim.g.maplocalleader = [[,]]
 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugs", {
+  defaults = { lazy = true, version = false },
+  checker = { notify = false },
+  change_detection = { enabled = false, notify = false },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
 require('options')
 require('actions')
 require('keymaps')
-require('plugins')
