7f5f1b244386aec109772751893a8d293f12bc57
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-08-23 07:26:27 -0600

    homerow: Configure hotkeys

--- home/.chezmoiscripts/run_onchange_04_configure_apps.sh
+++ home/.chezmoiscripts/run_onchange_04_configure_apps.sh
@@ -133,6 +133,15 @@ defaults write "co.noteplan.NotePlan3" "themeDark" '"Gruvbox iA.json"'
 echo "Homerow: Set shortcut to ctrl+f"
 defaults write "com.dexterleng.Homerow" "shortcut" '"\U2303F"'
 
+echo "Homerow: Set search shortcut to ctrl+shift+f"
+defaults write "com.dexterleng.Homerow" "search-shortcut" '"\U2303\U21e7F"'
+
+echo "Homerow: Set scroll shortcut to ctrl+g"
+defaults write "com.dexterleng.Homerow" "scroll-shortcut" '"\U2303G"'
+
+echo "Homerow: Enable experimental support for Spotify"
+defaults write "com.dexterleng.Homerow" "is-experimental-support-enabled" '1'
+
 echo "Homerow: Hide menubar icon"
 defaults write "com.dexterleng.Homerow" "NSStatusItem Visible Item-0" '0'
 defaults write "com.dexterleng.Homerow" "show-menubar-icon" '0'
