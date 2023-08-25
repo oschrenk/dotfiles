2db1bd918ccfa3604533437611b2e3d58267e54d
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-08-24 21:18:17 -0600

    nvim: spider plugin

---
+++ home/private_dot_config/nvim/lua/plugs/spider.lua
@@ -0,0 +1,39 @@
+return {
+  "chrisgrieser/nvim-spider",
+  event = "VeryLazy",
+  opts = true,
+  keys = {
+    {
+      "w",
+      function()
+        require("spider").motion "w"
+      end,
+      mode = { "n", "o", "x" },
+      desc = "Spider-w",
+    },
+    {
+      "e",
+      function()
+        require("spider").motion "e"
+      end,
+      mode = { "n", "o", "x" },
+      desc = "Spider-e",
+    },
+    {
+      "b",
+      function()
+        require("spider").motion "b"
+      end,
+      mode = { "n", "o", "x" },
+      desc = "Spider-b",
+    },
+    {
+      "ge",
+      function()
+        require("spider").motion "ge"
+      end,
+      mode = { "n", "o", "x" },
+      desc = "Spider-ge",
+    },
+  },
+}
