4e52e0d7173172979e4fc9f79e15a95663250f36
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-08-25 19:15:02 -0600

    nvim: mason.nvim

---
+++ home/private_dot_config/nvim/lua/plugs/mason.lua
@@ -0,0 +1,14 @@
+return {
+  "williamboman/mason.nvim",
+  config = function()
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      }
+    })
+  end,
+}
