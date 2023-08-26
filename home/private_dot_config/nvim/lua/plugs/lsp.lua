f1852118c92b701ad765c880df172177ccd76ca2
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-08-26 14:37:24 -0600

    nvim, lsp: Indent

--- home/private_dot_config/nvim/lua/plugs/lsp.lua
+++ home/private_dot_config/nvim/lua/plugs/lsp.lua
@@ -1,15 +1,17 @@
 return {
-  "williamboman/mason.nvim",
-   cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
-  config = function()
-    require("mason").setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
+  {
+    "williamboman/mason.nvim",
+    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
         }
-      }
-    })
-  end,
+      })
+    end,
+  }
 }
