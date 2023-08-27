3f299d4de9008b21c49e5f54ae0d103a22cc4211
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-08-27 10:11:30 -0600

    tmux, github_prs: Docs and error handling

--- home/private_dot_config/tmux/executable_github_prs.fish
+++ home/private_dot_config/tmux/executable_github_prs.fish
@@ -1,19 +1,64 @@
 #!/usr/bin/env fish
 
-set -x path_to_repo $argv[1]
+########################
+# README
+########################
 
-# return early if no path
-if test -z "$path_to_repo"
-  exit 0
+# Requirements:
+#   brew install gh
+#
+# Configuration:
+#   - gh needs to be authorized
+#   - needs to be enabled via `git config --local gh.query [QUERY]
+#     with QUERY something like
+#      `"is:open is:pr review-requested:oschrenk archived:false"`
+#   - optionally can configure how many tickets should be shown (defaults to 3)
+#     `git config --local gh.count [COUNT]
+
+# fail early if git not in $PATH
+if not type -q "git"
+  echo "Error: Could not find \"git\" in \$PATH" >&2
+  exit 1
 end
 
+# fail early if gh not in $PATH
+if not type -q "gh"
+  echo "Error: Could not find \"gh\" in \$PATH" >&2
+  exit 1
+end
+
+# set directory
+set -x path_to_repo $argv[1]
+
+# return early if not valid directory
+if not test -d "$path_to_repo"
+  echo "Error: \"$path_to_repo\" is not a valid path to a repo" >&2
+  exit 1
+end
+
+# fetch origin url
 set -x origin_url (git -C "$path_to_repo" config --get remote.origin.url 2>/dev/null)
 
 # return early if directory is not valid git
 if test $status -ne 0
-  exit 0
+  echo "Error: \"remote.origin.url\" is not configured." >&2
+  exit 1
 end
 
-set -x repo (echo $origin_url | cut -d ":" -f 2 | cut -d "." -f 1)
+set -x gh_query (git -C "$path_to_repo" config --local --get gh.query 2>/dev/null)
 
-gh pr -R $repo list --search "is:open is:pr review-requested:oschrenk archived:false" --json number | jq -r '.[] | .number = "\(.number)" | .number' | head -3 | awk -v d=" " '{s=(NR==1?s:s d)$0}END{print s}'
+# return early if we couldn't retrieve a github cli query
+if test $status -ne 0
+  echo "Error: \"gh.query\" is not configured." >&2
+  exit 1
+end
+
+set -x gh_count (git -C "$path_to_repo" config --local --get gh.count 2>/dev/null)
+
+# fall back onto default count of 3
+if test $status -ne 0
+  set -x gh_count 3
+end
+
+# execute
+gh pr -R "$origin_url" list --search "$gh_query" --json number | jq -r '.[] | .number = "\(.number)" | .number' | head -$gh_count | awk -v d=" " '{s=(NR==1?s:s d)$0}END{print s}'
