4de1198e6c9972b00131df8bba6eac9e11d74340
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-08-26 18:02:12 -0600

    launch-agents: Clarify

--- home/.chezmoiscripts/run_onchange_07_configure_launch_agents.sh
+++ home/.chezmoiscripts/run_onchange_07_configure_launch_agents.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 BASEDIR="$( chezmoi source-path )/../assets/agents"
-# launch agents
+
 for SERVICE in "com.oschrenk.homebrew"; do
+
+  # we rely on the filename being the name of the service
   FILE="$BASEDIR/$SERVICE.plist"
   # copy it to user's library
   echo "Copying $FILE to $HOME/Library/LaunchAgents"
   cp $FILE $HOME/Library/LaunchAgents
 
   # enable it
-  # we rely on the filename being the name of the service
-  # first cut removes ./modules/agents prefix
-  # seond cut removes .plist postfix
   USER_ID=$(id -u)
   TARGET_SPECIFIER="gui/$USER_ID/$SERVICE"
   echo "Enabling $SERVICE"
