ebdbbf8761972fb65c9e5a66acd6ec196457c423
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-09-05 14:21:33 -0600

    nvim: git-blame

---
+++ home/private_dot_config/nvim/lua/plugs/git-blame.lua
@@ -0,0 +1,8 @@
+return {
+  "f-person/git-blame.nvim",
+  event = { "BufRead" },
+  config = function()
+    vim.cmd "highlight default link gitblame SpecialComment"
+    vim.g.gitblame_enabled = 0
+  end,
+}
