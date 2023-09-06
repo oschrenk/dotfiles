97b88e2cc74fd821141c8ff4a0934e54ac74ad93
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-09-06 08:00:32 -0600

    fish: Use starship

--- home/private_dot_config/fish/config.fish
+++ home/private_dot_config/fish/config.fish
@@ -1,37 +1,14 @@
 if status --is-login
   source ~/.config/fish/env.fish
-
-  #  set -xg OPENAI_API_KEY (op read "op://Personal/auth0.openai.com/d3szyypowp4f2idhk2kebufl7e")
 end
 
 if status --is-interactive
-
   # Enable direnv
   eval (direnv hook fish)
 
   # source aliases
-  if test -r ~/.config/fish/aliases.fish
-    source ~/.config/fish/aliases.fish
-  end
+  source ~/.config/fish/aliases.fish
 
-
-  set -g theme_display_cmd_duration no
-  set -g theme_display_date no
-  set -g theme_display_git_ahead_verbose yes
-  set -g theme_display_git_dirty_verbose yes
-  set -g theme_display_git_stashed_verbose yes
-  set -g theme_display_git_untracked no
-  set -g theme_display_virtualenv no
-  set -g theme_nerd_fonts yes
-  set -g theme_newline_cursor clean
-  set -g theme_display_k8s_namespace no
-
-  # only show k8s context when in $HOME/Projects
-  function react_to_pwd --on-variable PWD
-    if string match -e -- "$HOME/Projects" "$PWD"
-      set -g theme_display_k8s_context   yes
-    else
-      set -g theme_display_k8s_context   no
-    end
-  end
+  # enable starship
+  starship init fish | source
 end
