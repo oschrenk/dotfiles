311db5c1c808f53830c68d468b65e197b009c245
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-10-02 15:35:17 +0200

    nvim, indent-blankline: Update to v3

--- home/private_dot_config/nvim/lua/plugs/indent-blankline.lua
+++ home/private_dot_config/nvim/lua/plugs/indent-blankline.lua
@@ -1,23 +1,26 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufNewFile", "BufRead", "BufAdd" },
+  main = "ibl",
   config = function()
-    require("indent_blankline").setup({
-      use_treesitter = true,
-      show_current_context = true,
-      show_trailing_blankline_indent = false,
-      space_char_blankline = " ",
-      char_list = { "|", "¦", "┆", "┊", "│", "│", "│", "│", "│", "│" },
-      context_char_list = { "|", "¦", "┆", "┊", "│", "│", "│", "│", "│", "│" },
-      buftype_exclude = {
-        "help",
-        "loclist",
-        "nofile",
-        "prompt",
-        "quickfix",
-        "terminal",
+    require("ibl").setup({
+      indent = {
+        char = { "|", "¦", "┆", "┊", "│", "│", "│", "│", "│", "│" },
       },
-      filetype_exclude = { "list" },
+      exclude = {
+        filetypes = {
+          "list"
+        },
+        buftypes = {
+          "help",
+          "loclist",
+          "nofile",
+          "prompt",
+          "quickfix",
+          "telescope",
+          "terminal",
+        },
+      },
     })
   end,
 }
