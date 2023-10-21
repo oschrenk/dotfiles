2849a7667158b23614411e97beae0bc2372663da
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-10-21 16:38:02 +0200

    macos: Configure keyboard brightness

--- home/.chezmoiscripts/run_onchange_04_configure_macos.sh
+++ home/.chezmoiscripts/run_onchange_04_configure_macos.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 #######################################
+# KEYBOARD
+#######################################
+
+# relies on https://github.com/oschrenk/keyboard.swift
+/opt/homebrew/bin/keyboard set --auto-brightness=true --idle-dim-time=10 --brightness=0
+
+#######################################
 # DOCK
 #######################################
 
