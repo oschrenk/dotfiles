0688bb37c66a18123a94b1533983064dc2850ae1
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-10-28 17:01:38 +0200

    nvim: Switch to caret

--- home/private_dot_config/nvim/lua/plugs/caret.lua
+++ home/private_dot_config/nvim/lua/plugs/caret.lua
@@ -1,5 +1,7 @@
 return {
   "projekt0n/caret.nvim",
+  lazy = false, -- make sure to load this during startup
+  priority = 1000, -- make sure to load this before all other plugins
   config = function()
     require("caret").setup({
       -- ...
