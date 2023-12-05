3f57d49eacc40d9b27ed730adcab154ea7d1b5dc
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-12-05 20:18:31 +0100

    fish: Format

--- home/private_dot_config/fish/config.fish
+++ home/private_dot_config/fish/config.fish
@@ -1,15 +1,15 @@
 set fish_greeting
 
 if status --is-login
-  source ~/.config/fish/env.fish
+    source ~/.config/fish/env.fish
 end
 
 if status --is-interactive
-  # Enable direnv
-  eval (direnv hook fish)
+    # Enable direnv
+    eval (direnv hook fish)
 
-  # source aliases
-  source ~/.config/fish/aliases.fish
+    # source aliases
+    source ~/.config/fish/aliases.fish
 
   # enable starship
   starship init fish | source
