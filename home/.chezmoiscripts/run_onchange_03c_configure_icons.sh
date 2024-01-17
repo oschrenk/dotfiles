84e769cd3b72a56b3d953e8020bd38595f9e074e
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-01-17 15:19:09 -0600

    icons: Quote BASEDIR

--- home/.chezmoiscripts/run_onchange_03c_configure_icons.sh
+++ home/.chezmoiscripts/run_onchange_03c_configure_icons.sh
@@ -19,9 +19,9 @@ if [ -d "/Applications/calibre.app" ]; then
   fi
 
   # overwrite image
-  sudo cp $BASEDIR/assets/icons/calibre.png /Applications/calibre.app/Contents/Resources/resources/images/library.png
+  sudo cp "$BASEDIR"/assets/icons/calibre.png /Applications/calibre.app/Contents/Resources/resources/images/library.png
   # use icon
-  sudo fileicon set /Applications/calibre.app $BASEDIR/assets/icons/calibre.icns
+  sudo fileicon set /Applications/calibre.app "$BASEDIR"/assets/icons/calibre.icns
 else
   echo "No calibre.app found"
 fi
@@ -43,7 +43,7 @@ if [ -d "$APP_DIR" ]; then
   # overwrite image
   sudo cp "$BASEDIR/assets/icons/jdownloader.png" "$ORIGINAL"
   # use icon
-  sudo fileicon set ~/Applications/JDownloader2.app/ $BASEDIR/assets/icons/jdownloader.icns
+  sudo fileicon set ~/Applications/JDownloader2.app/ "$BASEDIR"/assets/icons/jdownloader.icns
 else
   echo "No $APP_DIR found"
   echo
@@ -55,34 +55,34 @@ fi
 # fileicon compatible
 #--------------------------------------
 
-sudo fileicon set /Applications/Alacritty.app/ $BASEDIR/assets/icons/alacritty.icns
-sudo fileicon set /Applications/Arc.app $BASEDIR/assets/icons/arc.icns
-sudo fileicon set /Applications/Blackmagic\ ATEM\ Switchers/ATEM\ Software\ Control.app/ $BASEDIR/assets/icons/atem.icns
-sudo fileicon set /Applications/Docker.app $BASEDIR/assets/icons/docker.icns
+sudo fileicon set /Applications/Alacritty.app/ "$BASEDIR"/assets/icons/alacritty.icns
+sudo fileicon set /Applications/Arc.app "$BASEDIR"/assets/icons/arc.icns
+sudo fileicon set /Applications/Blackmagic\ ATEM\ Switchers/ATEM\ Software\ Control.app/ "$BASEDIR"/assets/icons/atem.icns
+sudo fileicon set /Applications/Docker.app "$BASEDIR"/assets/icons/docker.icns
 # docker has app inside app
-sudo fileicon set /Applications/Docker.app/Contents/MacOS/Docker\ Desktop.app $BASEDIR/assets/icons/docker.icns
+sudo fileicon set /Applications/Docker.app/Contents/MacOS/Docker\ Desktop.app "$BASEDIR"/assets/icons/docker.icns
 
-sudo fileicon set /Applications/Firefox.app/ $BASEDIR/assets/icons/firefox.icns
+sudo fileicon set /Applications/Firefox.app/ "$BASEDIR"/assets/icons/firefox.icns
 # Google Chat is in user app
-sudo fileicon set ~/Applications/Chrome\ Apps.localized/Google\ Chat.app $BASEDIR/assets/icons/google-chat.icns
-sudo fileicon set /Applications/Google\ Chrome.app $BASEDIR/assets/icons/google-chrome.icns
+sudo fileicon set ~/Applications/Chrome\ Apps.localized/Google\ Chat.app "$BASEDIR"/assets/icons/google-chat.icns
+sudo fileicon set /Applications/Google\ Chrome.app "$BASEDIR"/assets/icons/google-chrome.icns
 # the default icon is to similar to Noteplan 3, switch to a green icon
-sudo fileicon set /Applications/GoToMeeting.app/ $BASEDIR/assets/icons/google_meet.icns
-sudo fileicon set /Applications/HandBrake.app/ $BASEDIR/assets/icons/handbrake.icns
-sudo fileicon set /Applications/IntelliJ\ IDEA\ CE.app $BASEDIR/assets/icons/intellij.icns
-sudo fileicon set /Applications/OpenVPN\ Connect.app $BASEDIR/assets/icons/open-vpn.icns
-sudo fileicon set /Applications/Pulse\ Secure.app/ $BASEDIR/assets/icons/mozilla-vpn.icns
-sudo fileicon set /Applications/Sequel\ Pro.app/ $BASEDIR/assets/icons/sequel-pro.icns
-sudo fileicon set /Applications/Slack.app/ $BASEDIR/assets/icons/slack.icns
-sudo fileicon set /Applications/Spotify.app/ $BASEDIR/assets/icons/spotify.icns
-sudo fileicon set /Applications/Steam.app/ $BASEDIR/assets/icons/steam.icns
-sudo fileicon set /Applications/Telegram.app/ $BASEDIR/assets/icons/telegram.icns
-sudo fileicon set /Applications/The\ Hit\ List.app $BASEDIR/assets/icons/the-hit-list.icns
-sudo fileicon set /Applications/The\ Unarchiver.app $BASEDIR/assets/icons/the-unarchiver.icns
-sudo fileicon set /Applications/Transmission.app/ $BASEDIR/assets/icons/transmission.icns
-sudo fileicon set /Applications/Vimac.app/ $BASEDIR/assets/icons/vimac.icns
-sudo fileicon set /Applications/VLC.app/ $BASEDIR/assets/icons/vlc.icns
-sudo fileicon set /Applications/zoom.us.app $BASEDIR/assets/icons/zoom.icns
+sudo fileicon set /Applications/GoToMeeting.app/ "$BASEDIR"/assets/icons/google_meet.icns
+sudo fileicon set /Applications/HandBrake.app/ "$BASEDIR"/assets/icons/handbrake.icns
+sudo fileicon set /Applications/IntelliJ\ IDEA\ CE.app "$BASEDIR"/assets/icons/intellij.icns
+sudo fileicon set /Applications/OpenVPN\ Connect.app "$BASEDIR"/assets/icons/open-vpn.icns
+sudo fileicon set /Applications/Pulse\ Secure.app/ "$BASEDIR"/assets/icons/mozilla-vpn.icns
+sudo fileicon set /Applications/Sequel\ Pro.app/ "$BASEDIR"/assets/icons/sequel-pro.icns
+sudo fileicon set /Applications/Slack.app/ "$BASEDIR"/assets/icons/slack.icns
+sudo fileicon set /Applications/Spotify.app/ "$BASEDIR"/assets/icons/spotify.icns
+sudo fileicon set /Applications/Steam.app/ "$BASEDIR"/assets/icons/steam.icns
+sudo fileicon set /Applications/Telegram.app/ "$BASEDIR"/assets/icons/telegram.icns
+sudo fileicon set /Applications/The\ Hit\ List.app "$BASEDIR"/assets/icons/the-hit-list.icns
+sudo fileicon set /Applications/The\ Unarchiver.app "$BASEDIR"/assets/icons/the-unarchiver.icns
+sudo fileicon set /Applications/Transmission.app/ "$BASEDIR"/assets/icons/transmission.icns
+sudo fileicon set /Applications/Vimac.app/ "$BASEDIR"/assets/icons/vimac.icns
+sudo fileicon set /Applications/VLC.app/ "$BASEDIR"/assets/icons/vlc.icns
+sudo fileicon set /Applications/zoom.us.app "$BASEDIR"/assets/icons/zoom.icns
 
 echo "Killing Dock"
 killall Dock
