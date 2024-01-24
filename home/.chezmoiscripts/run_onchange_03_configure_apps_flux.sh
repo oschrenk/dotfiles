551bd9238ebc9f7d4ef3587e2f83b261044fdb1c
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-01-24 13:21:52 -0600

    chezmoiscripts: Extract Flux

--- home/.chezmoiscripts/run_onchange_03_configure_apps_flux.sh
+++ home/.chezmoiscripts/run_onchange_03_configure_apps_flux.sh
@@ -9,16 +9,23 @@
 source ./run_onchange_03_configure_apps__helper.sh
 
 #######################################
-# IntelliJ Idea CE
+# Flux
 #######################################
-echo "IntelliJ Idea CE: Enable key repeat"
-defaults write com.jetbrains.intellij.ce ApplePressAndHoldEnabled -bool false
+echo "Flux: Set location to Haarlem"
+defaults write "org.herf.Flux" "locationTextField" '"Haarlem"'
+defaults write "org.herf.Flux" "location" '"52.38,4.63"'
+
+echo "Flux: Set color transitions"
+defaults delete "org.herf.Flux" "dayColorTemp"
+defaults write "org.herf.Flux" "lateColorTemp" '1900'  # night
+defaults write "org.herf.Flux" "nightColorTemp" '3200' # evening
+defaults write "org.herf.Flux" "wakeTime" '390'
+defaults write "org.herf.Flux" "steptime" '26'
 
 ###########################################################
 # Kill affected applications                              #
 ###########################################################
-# Restarting cfprefsd and Finder to make keyboard changes stick
-for app in "idea" "cfprefsd" "Finder"; do
+for app in "Flux"; do
   while true; do
     read -p "Do you want to restart $app? [y/(n)]: " yn
     case $yn in
