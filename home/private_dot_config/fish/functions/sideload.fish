a147473b57f6817940eb0f4f744725d4491b5150
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-02-12 02:58:49 +0100

    fish, sideload: Include channelname

--- home/private_dot_config/fish/functions/sideload.fish
+++ home/private_dot_config/fish/functions/sideload.fish
@@ -1,3 +1,3 @@
 function sideload --description "Sideload to Castro"
-  yt-dlp --extract-audio --audio-format mp3 -o "$HOME/Library/Mobile Documents/iCloud~co~supertop~castro/Documents/Sideloads/%(title)s.%(ext)s" $argv
+    yt-dlp --extract-audio --audio-format mp3 -o "$HOME/Library/Mobile Documents/iCloud~co~supertop~castro/Documents/Sideloads/%(channel)s - %(title)s.%(ext)s" $argv
 end
