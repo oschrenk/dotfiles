4730e43842e6df3c4922427429ff51b067285056
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-04-20 10:49:08 +0200

    sketchybar, tmux: Use name only

--- home/private_dot_config/sketchybar/plugins/executable_tmux.attached.sh
+++ home/private_dot_config/sketchybar/plugins/executable_tmux.attached.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-session_raw=$(tmux list-sessions -F '#{session_name}:#{session_path}' -f "#{==:#{session_attached},1}")
-session_name=$(echo "$session_raw" | cut -d ":" -f 1)
-session_path=$(echo "$session_raw" | cut -d ":" -f 2)
+session_name=$(tmux list-sessions -F '#{session_name}' -f "#{==:#{session_attached},1}")
 
 if [ "$SENDER" = "tmux_session_update" ]; then
   if [ "$session_name" = "default" ]; then
