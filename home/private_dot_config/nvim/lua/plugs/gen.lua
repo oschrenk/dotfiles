29a5d0c9d61476fac5f46e062af3cb38ccf61e44
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-05-16 16:07:30 +0200

    nvim, gen: Try out gen

---
+++ home/private_dot_config/nvim/lua/plugs/gen.lua
@@ -0,0 +1,18 @@
+-- https://github.com/David-Kunz/gen.nvim
+-- interact with local llm
+return {
+  "David-Kunz/gen.nvim",
+  cmd = "Gen",
+  opts = {
+    -- see available models at https://ollama.com/library
+    model = "mistral", -- default model
+    host = "localhost", -- host running Ollama service.
+    port = "11434", -- port of Ollama service
+    retry_map = "<c-r>", -- set keymap to re-send the current prompt
+    display_mode = "split", -- display mode. Can be "float" or "split"
+    show_prompt = true, -- Show submitted prompt
+    show_model = true, -- Display used model
+    no_auto_close = true, -- Never auto-close window
+    debug = false, -- Print errors
+  },
+}
