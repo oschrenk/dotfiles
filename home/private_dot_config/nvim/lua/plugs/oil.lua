9e85be8cb480d459a87fa6dd90878aa11bd97608
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-05-16 12:24:51 +0200

    nvim: Add comments

--- home/private_dot_config/nvim/lua/plugs/oil.lua
+++ home/private_dot_config/nvim/lua/plugs/oil.lua
@@ -2,7 +2,11 @@
 -- edit filesystem like a buffer
 return {
   "stevearc/oil.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = {
+    -- https://github.com/nvim-tree/nvim-web-devicons
+    -- use icons from nerd fonts
+    "nvim-tree/nvim-web-devicons",
+  },
   opts = {},
   init = function()
     vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
