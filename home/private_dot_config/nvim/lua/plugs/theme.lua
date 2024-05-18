72e2cf38a17ad0159ed099efb43816d6e9c40bd3
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-05-18 15:02:38 +0200

    nvim, gruvbox: Set background colors

--- home/private_dot_config/nvim/lua/plugs/theme.lua
+++ home/private_dot_config/nvim/lua/plugs/theme.lua
@@ -1,20 +1,14 @@
--- return {
---   "projekt0n/caret.nvim",
---   lazy = false, -- make sure to load this during startup
---   priority = 1000, -- make sure to load this before all other plugins
---   config = function()
---     require("caret").setup({})
---     vim.o.background = "dark"
---     vim.cmd("colorscheme caret")
---   end,
--- }
 return {
   "ellisonleao/gruvbox.nvim",
   lazy = false, -- make sure to load this during startup
   priority = 1000, -- make sure to load this before all other plugins
   config = function()
-    -- load the colorscheme here
-    vim.cmd([[colorscheme gruvbox]])
-    vim.g["gruvbox_sign_column"] = "bg0"
+    require("gruvbox").setup({
+      overrides = {
+        SignColumn = { bg = "#282828" },
+        FoldColumn = { bg = "#282828" },
+      },
+    })
+    vim.cmd("colorscheme gruvbox")
   end,
 }
