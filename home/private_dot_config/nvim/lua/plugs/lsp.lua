7394326780a11b0bb728b11ee5b1a9e31c0215a8
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-05-20 21:15:12 +0200

    nvim, mason: Configure LSPs

--- home/private_dot_config/nvim/lua/plugs/lsp.lua
+++ home/private_dot_config/nvim/lua/plugs/lsp.lua
@@ -1,18 +1,25 @@
--- Setup
+-- Setup, "markdown"
 --
 -- It's important that you set up the plugins in the following order:
 -- 1. mason.nvim
 -- 2. mason-lspconfig.nvim
 -- 3. Setup servers via lspconfig
 
+-- Resources
+-- https://github.com/VonHeikemen/nvim-starter/blob/xx-mason/init.lua
+-- https://github.com/bluz71/dotfiles/blob/9e90fbb99e66501b374c8441f559d649788b028f/nvim/lua/config/lsp-config.lua#L108
+
+-- list of valid names
+-- https://raw.githubusercontent.com/neovim/nvim-lspconfig/master/doc/server_configurations.md
 local ensure_installed = {
-  "dockerfile-language-server",
-  "docker-compose-language-service",
+  "cssls",
+  "dockerls",
+  "eslint",
   "gopls",
-  "jq-lsp",
-  "lua-language-server",
+  "html",
+  "lua_ls",
   "marksman",
-  "typst-lsp",
+  "tsserver",
 }
 
 return {
@@ -25,18 +32,9 @@ return {
       "MasonUninstall",
       "MasonUninstallAll",
       "MasonUpdate",
-      "MasonUpdateAll",
     },
-    event = "User FileOpened",
     lazy = true,
     config = function(_, opts)
-      -- custom cmd to install/update all mason ensure_installed
-      vim.api.nvim_create_user_command("MasonUpdateAll", function()
-        if ensure_installed and #ensure_installed > 0 then
-          vim.cmd("MasonInstall " .. table.concat(ensure_installed, " "))
-        end
-      end, {})
-
       require("mason").setup({
         ui = {
           icons = {
@@ -52,12 +50,35 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     cmd = { "LspInstall", "LspUninstall" },
+    event = { "BufRead" },
     lazy = true,
-    event = "User FileOpened",
-    dependencies = "mason.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
     config = function()
-      require("mason-lspconfig").setup({
+      local lspconfig = require("lspconfig")
+      local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local mason_lspconfig = require("mason-lspconfig")
+      mason_lspconfig.setup({
         ensure_installed = ensure_installed,
+        handlers = {
+          function(server)
+            lspconfig[server].setup({
+              capabilities = lsp_capabilities,
+            })
+          end,
+          ["tsserver"] = function()
+            lspconfig.tsserver.setup({
+              capabilities = lsp_capabilities,
+              settings = {
+                completions = {
+                  completeFunctionCalls = true,
+                },
+              },
+            })
+          end,
+        },
       })
     end,
   },
