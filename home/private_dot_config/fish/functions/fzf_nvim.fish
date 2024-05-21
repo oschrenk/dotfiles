70c30a86dde942bcf4ebcbad33fca8eb5ffe6bb7
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-05-21 22:17:33 +0200

    fish: Fix file preview

--- home/private_dot_config/fish/functions/fzf_nvim.fish
+++ home/private_dot_config/fish/functions/fzf_nvim.fish
@@ -1,5 +1,5 @@
 function fzf_nvim --description "Open file in nvim"
-    eval "$FZF_CTRL_T_COMMAND" | fzf --ansi --no-sort --reverse --preview '~/.config/nvim/plugged/fzf.vim/bin/preview.sh {} | head -200' | while read -l r
+    eval "$FZF_CTRL_T_COMMAND" | fzf --ansi --no-sort --reverse --preview '~/.config/nvim/scripts/preview.sh {} | head -50' | while read -l r
         set result $result $r
     end
     if [ -z $result ]
