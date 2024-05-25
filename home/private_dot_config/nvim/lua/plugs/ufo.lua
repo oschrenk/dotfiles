77a1ea0d856fd055b631ecbcb5c223a8e76ee16f
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-05-25 08:17:46 +0200

    nvim, ufo: Prettify folds

--- home/private_dot_config/nvim/lua/plugs/ufo.lua
+++ home/private_dot_config/nvim/lua/plugs/ufo.lua
@@ -18,12 +18,34 @@ return {
   dependencies = {
     "kevinhwang91/promise-async",
     "nvim-treesitter/nvim-treesitter",
+    {
+      "luukvbaal/statuscol.nvim",
+      config = function()
+        local builtin = require("statuscol.builtin")
+        require("statuscol").setup({
+          -- whether to right-align the cursor line number with 'relativenumber' set
+          relculright = true,
+          segments = {
+            { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+            {
+              sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
+              click = "v:lua.ScSa",
+            },
+            { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+            {
+              sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+              click = "v:lua.ScSa",
+            },
+          },
+        })
+      end,
+    },
   },
   config = function()
     vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-    vim.o.foldcolumn = "0"
+    vim.o.foldcolumn = "1"
     vim.o.foldlevel = 99
-    vim.o.foldlevelstart = 100
+    vim.o.foldlevelstart = 99
     vim.o.foldenable = true
 
     require("ufo").setup({
