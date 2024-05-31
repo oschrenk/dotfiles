59fc7104cfd3b0017c911ab54c803d0ef3f2dce1
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-06-01 01:13:13 +0200

    nvim, metals: Fix goto definition

--- home/private_dot_config/nvim/lua/plugs/metals.lua
+++ home/private_dot_config/nvim/lua/plugs/metals.lua
@@ -38,7 +38,7 @@ return {
       vim.api.nvim_set_keymap(mode, lhs, rhs, options)
     end
 
-    map("n", "gD", "<cmd>lua vim.lsp.buf.definition()<CR>")
+    map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
     map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
     map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
     map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
