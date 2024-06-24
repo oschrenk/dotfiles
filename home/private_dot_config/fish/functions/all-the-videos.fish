4386b009a2201fb456098cc3ee36b6f5674dbf47
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-06-24 09:14:09 -0600

    fish: Download all youtube videos open in browser

---
+++ home/private_dot_config/fish/functions/all-the-videos.fish
@@ -0,0 +1,10 @@
+function all-the-videos --description "Dowloads all youtube videos open in browser"
+
+
+    export CHROME_BUNDLE_IDENTIFIER="company.thebrowser.Browser"
+
+    for vid in (chrome-cli list links | grep youtube | grep watch | cut -d " " -f2)
+        pad --no-playlist $vid
+    end
+
+end
