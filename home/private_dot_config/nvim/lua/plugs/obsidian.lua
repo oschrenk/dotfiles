4012dc8801821918dad15f644e69d9c26444f5bf
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-06-27 09:52:27 -0600

    nvim: Enable obsidian.nvim in all markdown

--- home/private_dot_config/nvim/lua/plugs/obsidian.lua
+++ home/private_dot_config/nvim/lua/plugs/obsidian.lua
@@ -58,7 +58,24 @@ return {
           },
         },
       },
+      -- enable osbidian.nvim for markdown outside of vault
+      -- see https://github.com/epwalsh/obsidian.nvim?tab=readme-ov-file#usage-outside-of-a-workspace-or-vault
+      {
+        name = "no-vault",
+        path = function()
+          return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+        end,
+        overrides = {
+          notes_subdir = vim.NIL,
+          new_notes_location = "current_dir",
+          templates = {
+            folder = vim.NIL,
+          },
+          disable_frontmatter = true,
+        },
+      },
     },
+
     ---@return table
     note_frontmatter_func = function(note)
       local out = {}
