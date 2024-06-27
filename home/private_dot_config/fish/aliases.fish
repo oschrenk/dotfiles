1076b0e3a150558f4ef0c2fecfbbffc768153b9a
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-06-27 16:06:03 -0600

    fish: Apply fish fmt

--- home/private_dot_config/fish/aliases.fish
+++ home/private_dot_config/fish/aliases.fish
@@ -1,7 +1,7 @@
 # Navigation
-alias ..    'cd ..'
-alias ...   'cd ../..'
-alias ....  'cd ../../..'
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
 alias ..... 'cd ../../../..'
 abbr -a -- - 'cd -'
 
@@ -9,11 +9,11 @@ alias d 'cd $HOME/Downloads'
 alias p 'cd $HOME/Projects'
 
 # Applications
-alias b 'brew'
-alias c 'chezmoi'
-alias k 'kubectl'
-alias r 'rg'
-alias t 'task'
+alias b brew
+alias c chezmoi
+alias k kubectl
+alias r rg
+alias t task
 
 alias ios 'open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app'
 
@@ -28,4 +28,4 @@ alias cux 'chmod u+x'
 alias noise 'play -q -c 2 --null synth brownnoise band -n 2500 4000 tremolo 20 .1 reverb 50'
 
 # Fun
-alias meow 'cat'
+alias meow cat
