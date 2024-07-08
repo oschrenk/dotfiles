d40ae9aa893a1fedf092cb782b1c5a5c4816f806
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-08 16:43:35 -0600

    sketchybar, mission: Show work tasks when in work mode

--- home/private_dot_config/sketchybar/plugins/executable_mission.sh
+++ home/private_dot_config/sketchybar/plugins/executable_mission.sh
@@ -12,7 +12,41 @@
 #   brew services restart mission
 #  This is because we are watching iCloud and system files (for macOS Focus)
 
-TASKS=$(/opt/homebrew/bin/mission tasks --show-done=false --show-cancelled=false)
+CURRENT_FOCUS=$(/opt/homebrew/bin/mission focus)
+
+# choose "work" tasks when in "work" mode
+if [[ "$CURRENT_FOCUS" == "com.apple.focus.work" ]]; then
+  TASKS=$(/opt/homebrew/bin/mission tasks --journal=work --show-done=false --show-cancelled=false)
+
+# default to "default" tasks, else
+else
+  TASKS=$(/opt/homebrew/bin/mission tasks --show-done=false --show-cancelled=false)
+
+fi
+
+case "$CURRENT_FOCUS" in
+  com.apple.donotdisturb.mode.default)
+    ICON="􀆺"
+    ICON_COLOR="0xFFFFFFFF"
+    ;;
+  com.apple.sleep.sleep-mode)
+    ICON="􀙪"
+    ICON_COLOR="0xFFFFFFFF"
+    ;;
+  com.apple.focus.personal-time)
+    ICON="􀉪"
+    ICON_COLOR="0xFFFFFFFF"
+    ;;
+  com.apple.focus.work)
+    ICON="􁕝"
+    ICON_COLOR="0xFFFFFFFF"
+    ;;
+  *)
+    ICON="􀟈"
+    ICON_COLOR="0xFF999999"
+    ;;
+esac
+  
 NEXT_TASK=$(echo "$TASKS" | head -1 | cut -d " " -f 2-)
 SUMMARY=$(echo "$TASKS" | tail -1 | cut -d " " -f 1)
 DONE=$(echo "$SUMMARY" | cut -d "/" -f 1)
@@ -20,12 +54,14 @@ TOTAL=$(echo "$SUMMARY" | cut -d "/" -f 2)
 
 if [[ "$DONE" != "$TOTAL" ]]; then
   sketchybar --set "$NAME" \
-               icon=󰸞 \
+               icon="$ICON" \
+               icon.color="$ICON_COLOR" \
                label="$NEXT_TASK $SUMMARY" \
                drawing=on
 else
   sketchybar --set "$NAME" \
-               icon=󰸞 \
+               icon="$ICON" \
+               icon.color="$ICON_COLOR" \
                label="No tasks" \
                drawing=on
 fi
