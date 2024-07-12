4d4ec844baf8ea25d10a0b00e8172200e00cdb68
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-12 12:51:40 +0200

    brew: Start ollama

--- home/.chezmoiscripts/run_onchange_05b_configure_brew_services.sh
+++ home/.chezmoiscripts/run_onchange_05b_configure_brew_services.sh
@@ -2,6 +2,6 @@
 
 # manually select services
 # for an overview do `brew services list`
-for SERVICE in "sketchybar" "mission"; do
+for SERVICE in "sketchybar" "mission" "ollama"; do
   brew services start "$SERVICE"
 done
