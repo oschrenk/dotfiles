9e94114357ad92c0ffde2933eb8d089e2a8e7131
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-15 22:49:48 +0200

    sketchybar, focus: Remove colors

--- home/private_dot_config/sketchybar/plugins/executable_focus.sh
+++ home/private_dot_config/sketchybar/plugins/executable_focus.sh
@@ -18,18 +18,18 @@ fi
 
 case "$status" in
   com.apple.donotdisturb.mode.default)
-    sketchybar -m --set focus icon.color=0xFFFFFFFF icon=􀆺
+    sketchybar -m --set focus icon=􀆺
     ;;
   com.apple.sleep.sleep-mode)
-    sketchybar -m --set focus icon.color=0xFFFFFFFF icon=􀙪
+    sketchybar -m --set focus icon=􀙪
     ;;
   com.apple.focus.personal-time)
-    sketchybar -m --set focus icon.color=0xFFFFFFFF icon=􀉪
+    sketchybar -m --set focus icon=􀉪
     ;;
   com.apple.focus.work)
-    sketchybar -m --set focus icon.color=0xFFFFFFFF icon=􁕝
+    sketchybar -m --set focus icon=􁕝
     ;;
   *)
-    sketchybar -m --set focus icon.color=0xFF999999 icon=􀟈
+    sketchybar -m --set focus icon=􀟈
     ;;
 esac
