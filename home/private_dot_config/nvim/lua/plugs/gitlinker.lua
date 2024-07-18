cde9aac38955fc6fd484714f92f659ec6e60de06
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-18 07:16:47 +0200

    nvim, gitlinker: Cleanup

--- home/private_dot_config/nvim/lua/plugs/gitlinker.lua
+++ home/private_dot_config/nvim/lua/plugs/gitlinker.lua
@@ -4,7 +4,6 @@
 return {
   "linrongbin16/gitlinker.nvim",
   cmd = "GitLink",
-  opts = {},
   keys = {
     { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
     { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
