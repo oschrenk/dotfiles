cfa0687a60b275879679a344de8809b75cacf7a3
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-19 18:33:15 +0200

    sketchybar, tmux.git: Fix showing icon

--- home/private_dot_config/sketchybar/plugins/executable_tmux.git.sh
+++ home/private_dot_config/sketchybar/plugins/executable_tmux.git.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
+# all other projects
+session_raw=$(tmux list-sessions -F '#{session_name}:#{session_path}' -f "#{==:#{session_attached},1}")
+session_name=$(echo "$session_raw" | cut -d ":" -f 1)
+session_path=$(echo "$session_raw" | cut -d ":" -f 2)
+
 # don't draw anything for default session
 if [ "$session_name" = "default" ]; then
   sketchybar --set "$NAME" drawing=off 
   exit 0
 fi
-
-# all other projects
-session_raw=$(tmux list-sessions -F '#{session_name}:#{session_path}' -f "#{==:#{session_attached},1}")
-session_name=$(echo "$session_raw" | cut -d ":" -f 1)
-session_path=$(echo "$session_raw" | cut -d ":" -f 2)
 
 # shorten the branch name to before slash
 # usually this this is the ticket number
