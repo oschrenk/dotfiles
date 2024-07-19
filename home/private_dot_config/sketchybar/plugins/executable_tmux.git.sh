6a39332bc0a4c3c3b45c33d296c5aebe1f01c66c
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-19 17:37:36 +0200

    sketchybar, tmux.git: Exit early

--- home/private_dot_config/sketchybar/plugins/executable_tmux.git.sh
+++ home/private_dot_config/sketchybar/plugins/executable_tmux.git.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+# only accept tmux_session_update events 
+if [ "$SENDER" != "tmux_session_update" ]; then
+  exit 0
+fi
+
+# don't draw anything for default session
+if [ "$session_name" = "default" ]; then
+  sketchybar --set "$NAME" drawing=off 
+  exit 0
+else 
+
+# all other projects
 session_raw=$(tmux list-sessions -F '#{session_name}:#{session_path}' -f "#{==:#{session_attached},1}")
 session_name=$(echo "$session_raw" | cut -d ":" -f 1)
 session_path=$(echo "$session_raw" | cut -d ":" -f 2)
 
-if [ "$SENDER" = "tmux_session_update" ]; then
-  if [ "$session_name" = "default" ]; then
-    sketchybar --set "$NAME" drawing=off 
-  else 
-    branch=$(git -C "$session_path" rev-parse --abbrev-ref HEAD)
-    sketchybar --set "$NAME" drawing=on icon.drawing=on icon=⎇  label="$branch"
-  fi
-fi
+branch=$(git -C "$session_path" rev-parse --abbrev-ref HEAD)
+
+sketchybar --set "$NAME" drawing=on icon.drawing=on icon=⎇  label="$branch"
