24574e171f6275b5659a0a089e60f1243badfc3c
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-21 14:39:25 +0200

    sketchybar, batter: Don't show percentage

--- home/private_dot_config/sketchybar/plugins/executable_battery.sh
+++ home/private_dot_config/sketchybar/plugins/executable_battery.sh
@@ -3,7 +3,7 @@
 PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(pmset -g batt | grep 'AC Power')
 
-if [ $PERCENTAGE = "" ]; then
+if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
@@ -23,6 +23,6 @@ if [[ $CHARGING != "" ]]; then
   ICON=""
 fi
 
-# The item invoking this script (name $NAME) will get its icon and label
-# updated with the current battery status
-sketchybar --set $NAME icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" \
+             icon="$ICON" \
+             label.drawing="off" 
