d361ea5eb493420674bebbac435bec5ae6309d9a
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-23 21:52:53 +0200

    sketchybar: Remove outdated tmux plugin

--- home/private_dot_config/sketchybar/plugins/executable_tmux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-session_raw=$(tmux list-sessions -F '#{session_name}:#{session_path}' -f "#{==:#{session_attached},1}")
-session_name=$(echo "$session_raw" | cut -d ":" -f 1)
-session_path=$(echo "$session_raw" | cut -d ":" -f 2)
-
-if [ "$SENDER" = "tmux_session_update" ]; then
-  sketchybar --set "$NAME" label="$session_name"
-fi
