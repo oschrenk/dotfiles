8f1abdf3c77b0be7c9368122e43530db9d2dd1d4
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-23 19:59:23 +0200

    nvim, oil: Customize config

--- home/private_dot_config/nvim/lua/plugs/oil.lua
+++ home/private_dot_config/nvim/lua/plugs/oil.lua
@@ -7,7 +7,32 @@ return {
     -- use icons from nerd fonts
     "nvim-tree/nvim-web-devicons",
   },
-  opts = {},
+  opts = {
+    -- disable default keymaps
+    use_default_keymaps = false,
+    keymaps = {
+      ["g?"] = "actions.show_help",
+      ["<CR>"] = "actions.select",
+      ["<C-p>"] = "actions.preview",
+      ["<C-c>"] = "actions.close",
+      ["<C-l>"] = "actions.refresh",
+      ["-"] = "actions.parent",
+      ["_"] = "actions.open_cwd",
+      ["gs"] = "actions.change_sort",
+      ["gx"] = "actions.open_external",
+      ["g."] = "actions.toggle_hidden",
+    },
+    -- Skip the confirmation popup for simple operations
+    -- simple means:
+    --  - no deletes
+    --  - at most one copy or move
+    --  - at most five creates
+    -- see also :help oil.skip_confirm_for_simple_edits
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = false,
+    },
+  },
   init = function()
     vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
   end,
