9e9153fe50c30639831516b20d3cf31527461491
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-24 19:03:12 +0200

    sketchybar, tmux: Rename event

--- home/private_dot_config/sketchybar/plugins/executable_tmux.windows.sh
+++ home/private_dot_config/sketchybar/plugins/executable_tmux.windows.sh
@@ -2,11 +2,11 @@
 
 # support up to 5 windows
 for id in $(seq 1 5); do
-  sketchybar --add item "tmux.windows.$id" left \
-	  	       --set tmux.windows."$id" \
+  sketchybar --add item "tmux.window.$id" left \
+	  	       --set tmux.window."$id" \
 		  	           script="$PLUGIN_DIR/tmux.window.sh" \
                    update_freq=60 \
 		         --add event tmux_windows_update \
-		         --subscribe tmux.windows."$id" tmux_windows_update
+		         --subscribe tmux.window."$id" tmux_windows_update
 done
 
