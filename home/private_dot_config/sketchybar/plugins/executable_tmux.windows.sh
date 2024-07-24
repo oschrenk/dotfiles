39b9de1ee2a4efa3811fcc2192de9aaabb4f93e1
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-24 18:59:25 +0200

    sketchybar, tmux: Update every 60s

--- home/private_dot_config/sketchybar/plugins/executable_tmux.windows.sh
+++ home/private_dot_config/sketchybar/plugins/executable_tmux.windows.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# support up to 3 windows
+# support up to 5 windows
 for id in $(seq 1 5); do
   sketchybar --add item "tmux.windows.$id" left \
 	  	       --set tmux.windows."$id" \
 		  	           script="$PLUGIN_DIR/tmux.window.sh" \
-                   update_freq=10 \
+                   update_freq=60 \
 		         --add event tmux_windows_update \
 		         --subscribe tmux.windows."$id" tmux_windows_update
 done
