0693477be1d8baef15a2c50427354bd6adadde47
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-29 23:19:40 +0200

    sketchybar, volume: Extract icons

--- home/private_dot_config/sketchybar/plugins/executable_volume.sh
+++ home/private_dot_config/sketchybar/plugins/executable_volume.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+ICON_SPEAKER_WAVE_3="󰕾"
+ICON_SPEAKER_WAVE_2="󰖀"
+ICON_SPEAKER_WAVE_1="󰕿"
+ICON_SPEAKER="󰖁"
+
 # The volume_change event supplies a $INFO variable in which the current volume
 # percentage is passed to the script.
 
@@ -7,13 +12,13 @@ if [ "$SENDER" = "volume_change" ]; then
   VOLUME=$INFO
 
   case $VOLUME in
-    [6-9][0-9]|100) ICON="󰕾"
+    [6-9][0-9]|100) ICON="$ICON_SPEAKER_WAVE_3"
     ;;
-    [3-5][0-9]) ICON="󰖀"
+    [3-5][0-9]) ICON="$ICON_SPEAKER_WAVE_2"
     ;;
-    [1-9]|[1-2][0-9]) ICON="󰕿"
+    [1-9]|[1-2][0-9]) ICON="$ICON_SPEAKER_WAVE_1"
     ;;
-    *) ICON="󰖁"
+    *) ICON="$ICON_SPEAKER"
   esac
 
   sketchybar --set "$NAME" \
