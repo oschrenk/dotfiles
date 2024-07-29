3acfd94b6ea449d4dc549776bf774a3fa67bdff0
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-29 23:13:25 +0200

    sketchybar, battery: Extract battery icons

--- home/private_dot_config/sketchybar/plugins/executable_battery.sh
+++ home/private_dot_config/sketchybar/plugins/executable_battery.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+ICON_BATTERY_100=""
+ICON_BATTERY_75=""
+ICON_BATTERY_50=""
+ICON_BATTERY_25=""
+ICON_BATTERY_0=""
+ICON_BATTERY_BOLT=""
+
 PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(pmset -g batt | grep 'AC Power')
 
@@ -8,19 +15,19 @@ if [ "$PERCENTAGE" = "" ]; then
 fi
 
 case ${PERCENTAGE} in
-  9[0-9]|100) ICON=""
+  9[0-9]|100) ICON="$ICON_BATTERY_100"
   ;;
-  [6-8][0-9]) ICON=""
+  [6-8][0-9]) ICON="$ICON_BATTERY_75"
   ;;
-  [3-5][0-9]) ICON=""
+  [3-5][0-9]) ICON="$ICON_BATTERY_50"
   ;;
-  [1-2][0-9]) ICON=""
+  [1-2][0-9]) ICON="$ICON_BATTERY_25"
   ;;
-  *) ICON=""
+  *) ICON="$ICON_BATTERY_0"
 esac
 
 if [[ $CHARGING != "" ]]; then
-  ICON=""
+  ICON="$ICON_BATTERY_BOLT"
 fi
 
 sketchybar --set "$NAME" \
