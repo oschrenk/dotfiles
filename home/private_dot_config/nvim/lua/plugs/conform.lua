e5ffd6c1b52da2f720a61bca13012a5e974354e1
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-31 14:25:43 +0200

    nvim, conform: fix typo

--- home/private_dot_config/nvim/lua/plugs/conform.lua
+++ home/private_dot_config/nvim/lua/plugs/conform.lua
@@ -39,7 +39,7 @@ return {
     formatters_by_ft = {
       fish = { "fish_indent" },
       go = { "gofmt" },
-      javascript = { "prettierd", "prettier", top_after_first = true },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
       -- brew install ktfmt
       kotlin = { "ktfmt" },
       lua = { "stylua" },
