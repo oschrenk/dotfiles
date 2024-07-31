1344f95c5550461602371a1d801af7d331948b68
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-31 11:54:07 +0200

    sketchybar, vpn: Color match

--- home/private_dot_config/sketchybar/plugins/executable_vpn.sh
+++ home/private_dot_config/sketchybar/plugins/executable_vpn.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+ICON_COLOR_ACTIVE="0xffcad3f5" 
+ICON_COLOR_INACTIVE="0xff484848"
+
 # Supported: 
 #   NextDNS
 #   NordVPN
@@ -9,8 +12,10 @@ VPN=$(scutil --nc list | grep Connected | sed -E 's/.*"(.*)".*/\1/')
 
 if [[ $VPN != "" ]]; then
   sketchybar -m --set vpn icon= \
+                          icon.color="$ICON_COLOR_ACTIVE" \
                           label="$VPN" 
 else
   sketchybar -m --set vpn icon= \
+                          icon.color="$ICON_COLOR_INACTIVE" \
                           label="$VPN" 
 fi
