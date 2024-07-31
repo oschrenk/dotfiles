c6a7d8147a1f8e0a978b4397183fb7eb7877eede
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-31 11:56:37 +0200

    sketchybar, tunnelblick: Color match

--- home/private_dot_config/sketchybar/plugins/executable_tunnelblick.sh
+++ home/private_dot_config/sketchybar/plugins/executable_tunnelblick.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+ICON_COLOR_ACTIVE="0xffcad3f5" 
+ICON_COLOR_INACTIVE="0xff484848"
+
 # Configuration states
 #   EXITING
 #   AUTH
@@ -17,15 +20,19 @@ fi
 # CONNECTED, EXITING
 STATE=$(osascript -e "tell application \"/Applications/Tunnelblick.app\"" -e "get state of configurations" -e "end tell")
 
+# disconnected by default
 DEFAULT_ICON="􀞝"
 ICON=$DEFAULT_ICON
+ICON_COLOR="$ICON_COLOR_INACTIVE"
 
 # if any connection is connected
 if [[ $STATE = "*"CONNECTED"*" ]]; then
   ICON="􀞛" 
+  ICON_COLOR="$ICON_COLOR_ACTIVE"
 fi
 
 sketchybar --set "$NAME" \
                icon="$ICON" \
+               icon.color="$ICON_COLOR" \
                label.drawing="off" 
 
