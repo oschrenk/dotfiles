28ab353d1432e93dc8718832c45459774b4d9e8b
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-31 11:52:21 +0200

    sketchybar, slack: Match color scheme better

--- home/private_dot_config/sketchybar/plugins/executable_slack.sh
+++ home/private_dot_config/sketchybar/plugins/executable_slack.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 BUNDLE_ID="com.tinyspeck.slackmacgap"
+ICON_COLOR_ACTIVE="0xffcad3f5" 
+ICON_COLOR_INACTIVE="0xff484848"
 
 BADGE=$(lsappinfo -all info -only StatusLabel "$BUNDLE_ID" | sed -nr 's/\"StatusLabel\"=\{ \"label\"=\"(.+)\" \}$/\1/p')
 
-ICON_COLOR=""0xffcad3f5""
+ICON_COLOR="$ICON_COLOR_ACTIVE"
 
 # Could also be `•` instead of a count
 if [[ -z "$BADGE"  || "$BADGE" == "•" ]]; then
-  ICON_COLOR="0xff646466"
+  ICON_COLOR="$ICON_COLOR_INACTIVE"
 fi
 
 sketchybar -m --set "$NAME" \
