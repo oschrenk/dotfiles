0d3f7c0bed119e0a8fb2ac948ddffe2b6b21a517
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-03 17:24:43 +0200

    sketchybar, lua: Stub Session.lua

---
+++ home/private_dot_config/sketchybar/items/Session.lua
@@ -0,0 +1,12 @@
+local sbar = require("sketchybar")
+
+local Session = {}
+
+-- @param icons Plugin specific icons
+function Session.new(icons)
+	local self = {}
+
+	return self
+end
+
+return Session
