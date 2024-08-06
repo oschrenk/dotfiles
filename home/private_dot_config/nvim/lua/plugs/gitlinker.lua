8267d1efad369c93acf82da8e8aac9a0f24072fe
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-06 22:58:51 +0200

    nvim, gitlinker: Fix missing empty opts

--- home/private_dot_config/nvim/lua/plugs/gitlinker.lua
+++ home/private_dot_config/nvim/lua/plugs/gitlinker.lua
@@ -3,7 +3,9 @@
 -- open directly
 return {
   "linrongbin16/gitlinker.nvim",
+  dependencies = { { "nvim-lua/plenary.nvim" } },
   cmd = "GitLink",
+  opts = {},
   keys = {
     { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
     { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
