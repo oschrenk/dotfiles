3cfcff085f3db9672691b943542b504ac2ab08b6
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-07 21:53:04 +0200

    sketchybar, lua, battery: No need to unset popup

--- home/private_dot_config/sketchybar/items/Battery.lua
+++ home/private_dot_config/sketchybar/items/Battery.lua
@@ -16,7 +16,6 @@ function Battery.new(icons)
 
     battery:subscribe("mouse.clicked", function(_)
       sbar.exec("open 'x-apple.systempreferences:com.apple.preference.battery'")
-      battery:set({ popup = { drawing = false } })
     end)
 
     battery:subscribe({ "power_source_change", "system_woke" }, function()
