139332e4c8c34807b451bdf99ddf58cd1bd0aa71
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-07 07:23:49 +0200

    sketchybar, weather: Switch to sf-symbols

--- home/private_dot_config/sketchybar/plugins/executable_weather.sh
+++ home/private_dot_config/sketchybar/plugins/executable_weather.sh
@@ -13,7 +13,7 @@ LOCATION="Haarlem,NL"
 # see https://github.com/chubin/wttr.in/tree/master?tab=readme-ov-file#one-line-output
 #%c is the weather as symbol
 #%t is the temperature
-FORMAT_STRING="+%c:+%t"
+FORMAT_STRING="+%x:+%t"
 #?m forces metrics units
 WEATHER_STRING=$(curl -s "https://wttr.in/${LOCATION}?m&format=${FORMAT_STRING}")
 
@@ -43,10 +43,115 @@ fi
 WEATHER_ICON=$(echo "$WEATHER_STRING" | cut -d ':' -f 1 | xargs)
 WEATHER_TEMP=$(echo "$WEATHER_STRING" | cut -d ':' -f 2 | tr -d '+')
 
+# SF-SYMBOLS
+CLOUD="􀇂"
+CLOUD_BOLT="􀇒"
+CLOUD_BOLT_RAIN="􀇞"
+CLOUD_DRIZZLE="􀇄"
+CLOUD_FILL="􀇃"
+CLOUD_FOG="􀇊"
+CLOUD_HEAVYRAIN="􀇈"
+CLOUD_RAIN="􀇆"
+CLOUD_RAINBOW_HALF="􁷞"
+CLOUD_SLEET="􀇐"
+CLOUD_SNOW="􀇎"
+CLOUD_SUN="􀇔"
+CLOUD_SUN_RAIN="􀇖"
+SLEET="􀇐"
+SNOWFLAKE="􀇥"
+SUN_MAX="􀆭"
+
+# WEATHER_SYMBOL_PLAIN
+# see https://github.com/chubin/wttr.in/blob/master/lib/constants.py#L150C1-L170C2
+case "$WEATHER_ICON" in
+   # "?": "Unknown"
+   '?')
+     # put a rainbow instead of a question mark
+     # unfortunately sf-symbols doesn't have a meteor glyph
+     ICON="$RAINBOW"
+   ;;
+   # "o"   : "Sunny"
+   'o')
+     ICON=""
+   ;;
+   # "=": "Fog"
+   '=')
+     ICON="$CLOUD_FOG"
+   ;;
+   # "///": "HeavyRain"
+   '///')
+     ICON="$CLOUD_HEAVYRAIN"
+   ;;
+   # "//": "HeavyShowers"
+   '//')
+     ICON="$CLOUD_RAIN"
+   ;;
+   # "/": "LightRain"
+   '/')
+     ICON="$CLOUD_SUN_RAIN"
+   ;;
+   # ".": "LightShowers"
+   '.')
+     ICON="$CLOUD_DRIZZLE"
+   ;;
+   # "**": "HeavySnow"
+   '**')
+     ICON="$SNOWFLAKE"
+   ;;
+   # "*/*": "HeavySnowShowers"
+   '*/*')
+     ICON="$SNOWFLAKE"
+   ;;
+   # "*": "LightSnow"
+   '*')
+     ICON="$CLOUD_SNOW"
+   ;;
+   # "*/": "LightSnowShowers"
+   '*/')
+     ICON="$CLOUD_SNOW"
+   ;;
+   # "x": "LightSleet"
+   'x')
+     ICON="$SLEET"
+   ;;
+   # "x/": "LightSleetShowers"
+   'x/')
+     ICON="$CLOUD_SLEET"
+   ;;
+   # "m": "PartlyCloudy"
+   'm')
+     ICON="$CLOUD_SUN"
+   ;;
+   # "mm": "Cloudy"
+   'mm')
+     ICON="$CLOUD"
+   ;;
+   # "mmm": "VeryCloudy"
+   'mmm')
+     ICON="$CLOUD_FILL"
+   ;;
+   # "/!/": "ThunderyHeavyRain"
+   '/!/')
+     ICON="$CLOUD_BOLT"
+   ;;
+   # "!/": "ThunderyShowers"
+   '!/')
+     ICON="$CLOUD_BOLT_RAIN"
+   ;;
+   # "*!*": "ThunderySnowShowers"
+   '*!*')
+     ICON="$CLOUD_BOLT_RAIN"
+   ;;
+   # anything else
+   "*") 
+     # it means wttr.in added a new symbol
+     ICON="x"
+   ;;
+esac
+
 sketchybar --set "$NAME" \
-           icon="$WEATHER_ICON" \
+           icon="$ICON" \
            icon.width="21" \
-           icon.y_offset=1 \
            icon.padding_right=0 \
            label.padding_left=0 \
            label="$WEATHER_TEMP"
