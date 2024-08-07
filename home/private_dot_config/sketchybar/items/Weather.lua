c43adccdf59ad10b125c84ae6631adba2a9b445a
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-07 23:11:46 +0200

    sketchybar, lua, weather: Emoji based weather

--- home/private_dot_config/sketchybar/items/Weather.lua
+++ home/private_dot_config/sketchybar/items/Weather.lua
@@ -1,3 +1,5 @@
+require("utils.Strings")
+
 local sbar = require("sketchybar")
 
 local Weather = {}
@@ -6,6 +8,13 @@ local Weather = {}
 function Weather.new(icons)
   local self = {}
 
+  local Location = "Haarlem,NL"
+
+  -- see https://github.com/chubin/wttr.in/tree/master?tab=readme-ov-file#one-line-output
+  -- %c is the weather as emoji
+  -- %t is the temperature
+  local FormatString = "+%c:+%t"
+
   self.add = function(position)
     local weather = sbar.add("item", {
       position = position,
@@ -13,8 +22,20 @@ function Weather.new(icons)
       icon = icons.unknown,
     })
 
+    local update = function()
+      local cmd = string.format("curl -s 'https://wttr.in/%s?m&format=%s'", Location, FormatString)
+
+      sbar.exec(cmd, function(oneline)
+        local resp = Split(oneline, ":")
+        local icon = resp[1]:gsub("%s+", "")
+        local label = resp[2]:gsub("%s+", "")
+
+        weather:set({ icon = icon, label = label })
+      end)
+    end
+
     weather:subscribe({ "forced", "routine", "system_woke" }, function(_)
-      weather:set({ label = "weather" })
+      update()
     end)
   end
 
