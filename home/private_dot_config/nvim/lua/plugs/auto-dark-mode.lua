a146ac832ef9f9cc0069bc0f7c8a01dc69f12bd6
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-11 10:40:49 +0200

    nvim, theme: Setup sign/fold column colors for light mode

--- home/private_dot_config/nvim/lua/plugs/auto-dark-mode.lua
+++ home/private_dot_config/nvim/lua/plugs/auto-dark-mode.lua
@@ -6,10 +6,22 @@ return {
     update_interval = 5000,
     set_dark_mode = function()
       vim.api.nvim_set_option_value("background", "dark", {})
+      require("gruvbox").setup({
+        overrides = {
+          SignColumn = { bg = "#282828" },
+          FoldColumn = { bg = "#282828" },
+        },
+      })
       vim.cmd("colorscheme gruvbox")
     end,
     set_light_mode = function()
       vim.api.nvim_set_option_value("background", "light", {})
+      require("gruvbox").setup({
+        overrides = {
+          SignColumn = { bg = "#f9f1cb" },
+          FoldColumn = { bg = "#f9f1cb" },
+        },
+      })
       vim.cmd("colorscheme gruvbox")
     end,
   },
