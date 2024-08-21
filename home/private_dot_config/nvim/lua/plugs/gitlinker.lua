8365a14c49f2b2ad8d129426835ec72e5e821903
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-21 13:53:38 +0200

    nvim, gitlinker: Add git repo

--- home/private_dot_config/nvim/lua/plugs/gitlinker.lua
+++ home/private_dot_config/nvim/lua/plugs/gitlinker.lua
@@ -10,4 +10,19 @@ return {
     { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
     { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
   },
+  config = function()
+    require("gitlinker").setup({
+      router = {
+        browse = {
+          ["git.timewax.com"] = "https://git.timewax.com/"
+            .. "{_A.ORG}/"
+            .. "{_A.REPO}/blob/"
+            .. "{_A.REV}/"
+            .. "{_A.FILE}"
+            .. "#L{_A.LSTART}"
+            .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
+        },
+      },
+    })
+  end,
 }
