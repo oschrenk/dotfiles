786780ba9abdb57c381755d057a1f03046b7e6f1
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-23 22:59:05 +0200

    nvim, markdown: disable conceal

--- home/private_dot_config/nvim/ftplugin/markdown.lua
+++ home/private_dot_config/nvim/ftplugin/markdown.lua
@@ -1,2 +1,10 @@
 vim.opt.spell = true
-vim.opt_local.conceallevel = 2
+
+local path = vim.api.nvim_buf_get_name(0)
+
+-- very dumb test to see if we are in obsidian vault
+if string.find(path, "obsidian") then
+  vim.opt_local.conceallevel = 2
+else
+  vim.opt_local.conceallevel = 0
+end
