4e4da9c81102192bfeb6f3ef1d3fb40a122fbeef
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-25 15:38:22 +0200

    nvim, git-conflict: Define colors

--- home/private_dot_config/nvim/lua/plugs/git-conflict.lua
+++ home/private_dot_config/nvim/lua/plugs/git-conflict.lua
@@ -15,5 +15,12 @@ return {
     default_mappings = true,
     default_commands = true,
     disable_diagnostics = true,
+    highlights = {
+      -- see also
+      -- https://github.com/ellisonleao/gruvbox.nvim/blob/7a1b23e4edf73a39642e77508ee6b9cbb8c60f9e/lua/gruvbox.lua#L364
+      current = "DiffAdd",
+      incoming = "DiffText",
+      ancestor = "DiffChange",
+    },
   },
 }
