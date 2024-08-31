9150dcc92c2a6e2da6021f6738a0cd93c93a0ec2
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-31 10:04:04 +0200

    nvim: avante

---
+++ home/private_dot_config/nvim/lua/plugs/avante.lua
@@ -0,0 +1,70 @@
+-- AI-driven code suggestions
+-- https://github.com/yetone/avante.nvim
+--
+-- get api keys via
+-- https://console.anthropic.com/settings/keys
+return {
+  "yetone/avante.nvim",
+  opts = {
+    -- add any opts here
+  },
+  keys = {
+    {
+      "<leader>aa",
+      function()
+        require("avante.api").ask()
+      end,
+      desc = "avante: ask",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>ar",
+      function()
+        require("avante.api").refresh()
+      end,
+      desc = "avante: refresh",
+    },
+    {
+      "<leader>ae",
+      function()
+        require("avante.api").edit()
+      end,
+      desc = "avante: edit",
+      mode = "v",
+    },
+  },
+  dependencies = {
+    -- https://github.com/nvim-lua/plenary.nvim
+    "nvim-lua/plenary.nvim",
+    -- https://github.com/stevearc/dressing.nvim
+    "stevearc/dressing.nvim",
+    -- https://github.com/MunifTanjim/nui.nvim
+    "MunifTanjim/nui.nvim",
+    -- "nvim-tree/nvim-web-devicons"
+    "nvim-tree/nvim-web-devicons", -- optional
+    {
+      -- support for image pasting
+      -- https://github.com/HakonHarnes/img-clip.nvim
+      "HakonHarnes/img-clip.nvim",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
