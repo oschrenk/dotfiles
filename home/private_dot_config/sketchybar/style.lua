a817b7528d68922bee0ec77c294f09c2269c364a
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-09-02 10:55:49 +0200

    sketchybar: Move defaults to style

--- home/private_dot_config/sketchybar/style.lua
+++ home/private_dot_config/sketchybar/style.lua
@@ -3,10 +3,6 @@ local white = 0xffcad3f5
 local grey = 0xff484848
 
 return {
-  background = {
-    color = black,
-    corner_radius = 5,
-  },
   bar = {
     margin = 0,
     color = black,
@@ -28,31 +24,37 @@ return {
     -- main, all, <positive_integer list>
     display = "all",
   },
-  icon = {
-    font = {
-      -- brew install oschrenk/casks/font-sf-mono-nerd-font
-      family = "SFMono Nerd Font",
-      style = "Medium",
-      size = 15.0,
+  default = {
+    icon = {
+      font = {
+        -- brew install oschrenk/casks/font-sf-mono-nerd-font
+        family = "SFMono Nerd Font",
+        style = "Medium",
+        size = 15.0,
+      },
+      color = white,
+      width = 30,
+      align = "center",
+      padding_left = 2,
+      padding_right = 2,
+      y_offset = 0,
     },
-    color = white,
-    width = 30,
-    align = "center",
-    padding_left = 2,
-    padding_right = 2,
-    y_offset = 0,
-  },
-  label = {
-    font = {
-      -- brew install font-sf-pro
-      family = "SF Pro",
-      style = "Regular",
-      size = 14.0,
+    label = {
+      font = {
+        -- brew install font-sf-pro
+        family = "SF Pro",
+        style = "Regular",
+        size = 14.0,
+      },
+      color = white,
+      padding_left = 3,
+      padding_right = 3,
+      y_offset = 0,
     },
-    color = white,
-    padding_left = 3,
-    padding_right = 3,
-    y_offset = 0,
+    background = {
+      color = black,
+      corner_radius = 5,
+    },
   },
   sessions = {
     active = white,
