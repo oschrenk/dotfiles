5e4b861937d1a4a1e3661731f9122d29dd99744b
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-09-15 08:20:41 +0200

    sketchybar: Watch for changes

--- home/private_dot_config/sketchybar/items/Calendar.lua
+++ home/private_dot_config/sketchybar/items/Calendar.lua
@@ -17,78 +17,78 @@ local sbar = require("sketchybar")
 -- beyond that date and time calculation might be wrong
 local Calendar = {}
 function Calendar.new(icons, focus)
-	local self = {}
+  local self = {}
 
-	self.add = function(position)
-		local calendar = sbar.add("item", {
-			position = position,
-			update_freq = 120,
-			icon = icons.default,
-		})
+  self.add = function(position)
+    local calendar = sbar.add("item", {
+      position = position,
+      update_freq = 120,
+      icon = icons.default,
+    })
 
-		local update = function()
-			sbar.exec("/opt/homebrew/bin/plan next --ignore-tags timeblock --ignore-all-day-events", function(json)
-				local event = json[1]
-				if event ~= nil then
-					local icon = event.title.icon
-					local label = event.title.label
-					local suffix = ""
+    local update = function()
+      sbar.exec("/opt/homebrew/bin/plan next --ignore-tags timeblock --ignore-all-day-events", function(json)
+        local event = json[1]
+        if event ~= nil then
+          local icon = event.title.icon
+          local label = event.title.label
+          local suffix = ""
 
-					if event["schedule"]["start"]["in"] < 0 then
-						suffix = ", " .. event["schedule"]["end"]["in"] .. "m" .. " left"
-					else
-						suffix = " in " .. event["schedule"]["start"]["in"] .. "m"
-					end
-					calendar:set({
-						icon = {
-							string = icon,
-							drawing = true,
-						},
-						label = {
-							string = label .. suffix,
-							drawing = true,
-						},
-						drawing = true,
-					})
+          if event["schedule"]["start"]["in"] < 0 then
+            suffix = ", " .. event["schedule"]["end"]["in"] .. "m" .. " left"
+          else
+            suffix = " in " .. event["schedule"]["start"]["in"] .. "m"
+          end
+          calendar:set({
+            icon = {
+              string = icon,
+              drawing = true,
+            },
+            label = {
+              string = label .. suffix,
+              drawing = true,
+            },
+            drawing = true,
+          })
 
-					local url = ""
-					for type, u in pairs(event["services"]) do
-						if type == "ical" then
-							url = u
-						end
-					end
-					calendar:subscribe("mouse.clicked", function(_)
-						sbar.exec("open '" .. url .. "'")
-					end)
-				else
-					calendar:set({
-						icon = {
-							string = icons.default,
-							drawing = true,
-						},
-						label = {
-							drawing = false,
-						},
-						drawing = true,
-					})
-				end
-			end)
-		end
+          local url = ""
+          for type, u in pairs(event["services"]) do
+            if type == "ical" then
+              url = u
+            end
+          end
+          calendar:subscribe("mouse.clicked", function(_)
+            sbar.exec("open '" .. url .. "'")
+          end)
+        else
+          calendar:set({
+            icon = {
+              string = icons.default,
+              drawing = true,
+            },
+            label = {
+              drawing = false,
+            },
+            drawing = true,
+          })
+        end
+      end)
+    end
 
-		local onComplete = function(current_focus)
-			if current_focus == focus.dnd or current_focus == focus.sleep then
-				calendar:set({ drawing = false })
-			else
-				update()
-			end
-		end
+    local onComplete = function(current_focus)
+      if current_focus == focus.dnd or current_focus == focus.sleep then
+        calendar:set({ drawing = false })
+      else
+        update()
+      end
+    end
 
-		calendar:subscribe({ "forced", "routine", "system_woke", "mission_focus" }, function(_)
-			focus.handler(onComplete)
-		end)
-	end
+    calendar:subscribe({ "forced", "routine", "system_woke", "mission_focus", "calendar_changed" }, function(_)
+      focus.handler(onComplete)
+    end)
+  end
 
-	return self
+  return self
 end
 
 return Calendar
