f9bd86a90f510470218cc3f9f5bbb085cb19a5b9
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-09-19 08:07:58 +0200

    nvim: Configure markdown

---
+++ home/private_dot_config/nvim/lua/plugs/markdown.lua
@@ -0,0 +1,12 @@
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    file_types = { "markdown", "Avante" },
+  },
+  ft = { "markdown", "Avante" },
+}
--- home/private_dot_config/nvim/lua/plugs/obsidian.lua
+++ home/private_dot_config/nvim/lua/plugs/obsidian.lua
@@ -109,12 +109,7 @@ return {
       },
     },
     ui = {
-      checkboxes = {
-        -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
-        [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-        ["x"] = { char = "", hl_group = "ObsidianDone" },
-        ["-"] = { char = "󰅗", hl_group = "ObsidianTilde" },
-      },
+      enable = false,
     },
   },
 }
