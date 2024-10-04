5b75f9aa70ec3a62e65cd294c975218d2d907e77
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-10-04 10:25:02 +0200

    sketchybar, util: get wifi name

--- home/private_dot_config/sketchybar/utils/System.lua
+++ home/private_dot_config/sketchybar/utils/System.lua
@@ -1,6 +1,21 @@
 local _M = {}
+
+-- Synchronously fetch current hostname
+--
+-- Not suited to be called regularly
 function _M.getHostname()
   local f = assert(io.popen("/bin/hostname"))
+  local hostname = f:read("*a") or ""
+  f:close()
+  hostname = string.gsub(hostname, "\n$", "")
+  return hostname
+end
+
+-- Synchronously fetch current Wifi
+--
+-- Not suited to be called regularly
+function _M.getWifi()
+  local f = assert(io.popen("networksetup -getairportnetwork en0 | awk '{print $4}' | head -n 1"))
   local hostname = f:read("*a") or ""
   f:close()
   hostname = string.gsub(hostname, "\n$", "")
