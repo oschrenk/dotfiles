6871dcdf8b3ef73c01216a81d22e15d8b579a2ef
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-10-04 10:07:47 +0200

    sketchybar: assert non nil

--- home/private_dot_config/sketchybar/utils/System.lua
+++ home/private_dot_config/sketchybar/utils/System.lua
@@ -1,9 +1,10 @@
 local _M = {}
 function _M.getHostname()
-  local f = io.popen("/bin/hostname")
+  local f = assert(io.popen("/bin/hostname"))
   local hostname = f:read("*a") or ""
   f:close()
   hostname = string.gsub(hostname, "\n$", "")
   return hostname
 end
+
 return _M
