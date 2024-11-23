7ad42ff8c267530e9fb44a6827c45b465ceaf911
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-11-23 21:31:15 +0100

    nvim, neovide: Allow pasting with cmd+v

--- home/private_dot_config/nvim/lua/neovide.lua
+++ home/private_dot_config/nvim/lua/neovide.lua
@@ -5,4 +5,9 @@ if vim.g.neovide then
   vim.g.neovide_position_animation_length = 0
   vim.g.neovide_scroll_animation_far_lines = 0
   vim.g.neovide_scroll_animation_length = 0
+
+  -- allow pasting with cmd+v
+  vim.keymap.set({ "n", "v", "s", "x", "o", "i", "l", "c", "t" }, "<D-v>", function()
+    vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
+  end, { noremap = true, silent = true })
 end
