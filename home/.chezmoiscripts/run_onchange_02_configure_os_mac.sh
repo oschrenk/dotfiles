f58d2f8cd3c494af2c5eab7313c0aad70bac19e6
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-02-02 11:15:14 +0100

    config, os: Split off finder

--- home/.chezmoiscripts/run_onchange_02_configure_os_mac.sh
+++ home/.chezmoiscripts/run_onchange_02_configure_os_mac.sh
@@ -3,7 +3,6 @@
 #######################################
 # Available Sections:
 #
-# FINDER
 # DESKTOP
 # MENUBAR
 # SPOTLIGHT
@@ -14,84 +13,6 @@
 # ACCESSIBILITY
 #
 #######################################
-
-#######################################
-# FINDER
-#######################################
-
-echo "Finder: Disable the warning when changing a file extension"
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-# Finder: new window location set to $HOME/Downloads. Same as Finder > Preferences > New Finder Windows show
-# Computer     : `PfCm`
-# Volume       : `PfVo`
-# $HOME        : `PfHm`
-# Desktop      : `PfDe`
-# Documents    : `PfDo`
-# All My Files : `PfAF`
-# Other…       : `PfLo`
-# eg. For $HOME use "PfHm" and "file://${HOME}/"
-# eg For other path use "PfLo" and "file:///foo/bar/"
-echo "Finder: Set new window location to ~/Downloads"
-defaults write com.apple.finder NewWindowTarget -string "PfLo"
-defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/Downloads"
-
-# Toggles menu item View > "Show/Hide Path Bar"
-# Requires closing of finder windows
-# Tested on macOS 12.4
-echo "Finder: Show Path bar in Finder"
-defaults write com.apple.finder ShowPathbar -bool true
-
-# Hide Statusbar
-# Toggles menu item View > "Show/Hide Status Bar"
-# Requires closing of finder windows
-# Tested on macOS 12.4
-echo "Finder: Show Status bar in Finder"
-defaults write com.apple.finder 'ShowStatusBar' -bool false
-
-# Toggles "Finder > Preferences > Advanced > "Show all filename extensions"
-# Tested on macOS 12.4
-# Requires 'killall Finder'"
-echo "Finder: Show all filename extensions in Finder"
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-# Set search scope.
-# This Mac       : `SCev`
-# Current Folder : `SCcf`
-# Previous Scope : `SCsp`
-# Tested on macOS 12.4
-# Requires 'killall Finder'"
-echo "Finder: Set search scope to current folder"
-defaults write com.apple.finder FXDefaultSearchScope SCcf
-
-# Set preferred view style.
-# Icon View   : `icnv`
-# List View   : `Nlsv`
-# Column View : `clmv`
-# Cover Flow  : `Flwv`
-# Tested on macOS 12.4
-# Requires deletion of ~/.DS_Store
-echo "Finder: Set preferred view style to column view"
-defaults write com.apple.finder FXPreferredViewStyle clmv
-rm -rf ~/.DS_Store
-
-# Set width of sidebar
-# Tested on macOS 12.4
-# Requires 'killall Finder'"
-echo "Finder: Set sidebar width to 150"
-defaults write com.apple.finder SidebarWidth -int 150
-
-# Enable spring loading for directories
-# Tested on macOS 12.5
-# Requires: Nothing. Takes immediate effect
-echo "Finder: Enable spring loading for directories"
-defaults write NSGlobalDomain com.apple.springing.enabled -bool true
-
-# Shorten the spring loading delay for directories
-# Tested on macOS 12.5
-# Requires: Nothing. Takes immediate effect
-echo "Finder: Shorten the spring loading delay for directories"
-defaults write NSGlobalDomain com.apple.springing.delay -float 0.2
 
 #######################################
 # DESKTOP
