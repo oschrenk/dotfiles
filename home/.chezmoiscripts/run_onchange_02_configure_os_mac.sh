3cae7702d799114e11f8866ab2102ae54ce245b2
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-02-02 11:23:41 +0100

    config, os: Split off keyboard

--- home/.chezmoiscripts/run_onchange_02_configure_os_mac.sh
+++ home/.chezmoiscripts/run_onchange_02_configure_os_mac.sh
@@ -3,45 +3,10 @@
 #######################################
 # Available Sections:
 #
-# KEYBOARD
 # TIME MACHINE
 # ACCESSIBILITY
 #
 #######################################
-
-#######################################
-# KEYBOARD
-#######################################
-
-echo "Keyboard: Set key repeat rate to 1 (normal minimum is 2 => 30 ms)"
-echo "To experiment with speeds go to https://mac-os-key-repeat.vercel.app/"
-defaults write "Apple Global Domain" "KeyRepeat" '1'
-
-echo "Keyboard: Set initial key repeat rate to 3 (normal minimum is 3 => 225ms)"
-echo "To experiment with speeds go to https://mac-os-key-repeat.vercel.app/"
-defaults write "Apple Global Domain" "InitialKeyRepeat" '3'
-
-echo "Keyboard: Disable auto capitalization"
-defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
-
-echo "Keyboard: Disable \"smart\" dashes"
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-
-echo "Keyboard: Disable automatic period substitutions"
-defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
-
-echo "Keyboard: Disable smart quotes"
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-
-echo "Keyboard: Enable keyboard navigation"
-defaults write "Apple Global Domain" "AppleKeyboardUIMode" '2'
-
-echo "Keyboard: Fn does nothing"
-defaults write "com.apple.HIToolbox" "AppleFnUsageType" '0'
-
-echo "Keyboard shortcut: Disable ^→, and ^← to switch spaces"
-plutil -replace AppleSymbolicHotKeys.79.enabled -bool NO ~/Library/Preferences/com.apple.symbolichotkeys.plist
-plutil -replace AppleSymbolicHotKeys.81.enabled -bool NO ~/Library/Preferences/com.apple.symbolichotkeys.plist
 
 #######################################
 # TIME MACHINE
