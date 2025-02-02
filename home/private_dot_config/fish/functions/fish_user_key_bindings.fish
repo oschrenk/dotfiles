f22c1f55eddf53acb553650f30f79231389d4f10
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-02-02 10:45:24 +0100

    fish: Unbind -

--- home/private_dot_config/fish/functions/fish_user_key_bindings.fish
+++ home/private_dot_config/fish/functions/fish_user_key_bindings.fish
@@ -4,3 +4,8 @@ bind \cp fzf_projects
 bind \co fzf_nvim
 bind \cg fzf_commits
 bind \cb fzf_branches
+bind \ct fzf_datepicker
+
+# fish 4.x binds ctrl+dash
+#  which clases with my tmux binding for h-split
+bind --erase --preset ctrl-_
