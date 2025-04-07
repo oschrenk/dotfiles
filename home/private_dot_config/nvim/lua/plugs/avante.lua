b5ae91aaad7b40fe2bdcc9aec55710087d04fa4d
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-04-07 22:22:18 +0200

    nvim, avante: Configure Claude key via password manager

--- home/private_dot_config/nvim/lua/plugs/avante.lua
+++ home/private_dot_config/nvim/lua/plugs/avante.lua
@@ -5,10 +5,20 @@
 -- https://console.anthropic.com/settings/keys
 return {
   "yetone/avante.nvim",
-  opts = {
-    -- add any opts here
-  },
   build = "make",
+  dependencies = {
+    -- https://github.com/nvim-lua/plenary.nvim
+    "nvim-lua/plenary.nvim",
+    -- https://github.com/stevearc/dressing.nvim
+    "stevearc/dressing.nvim",
+    -- https://github.com/MunifTanjim/nui.nvim
+    "MunifTanjim/nui.nvim",
+    -- "nvim-tree/nvim-web-devicons"
+    "nvim-tree/nvim-web-devicons", -- optional
+    {
+      -- see markdown setup in markdown.nvim
+    },
+  },
   keys = {
     {
       "<leader>aa",
@@ -34,17 +44,21 @@ return {
       mode = "v",
     },
   },
-  dependencies = {
-    -- https://github.com/nvim-lua/plenary.nvim
-    "nvim-lua/plenary.nvim",
-    -- https://github.com/stevearc/dressing.nvim
-    "stevearc/dressing.nvim",
-    -- https://github.com/MunifTanjim/nui.nvim
-    "MunifTanjim/nui.nvim",
-    -- "nvim-tree/nvim-web-devicons"
-    "nvim-tree/nvim-web-devicons", -- optional
-    {
-      -- see markdown setup in markdown.nvim
+  opts = {
+    provider = "claude",
+    claude = {
+      -- export ANTHROPIC_API_KEY="..."
+      -- security add-generic-password -s "avante-claude-api" -a "ANTHROPIC_API_KEY" -w "$ANTHROPIC_API_KEY"
+      -- if api_key_name is a table, it's treated like a command
+      api_key_name = {
+        "/usr/bin/security",
+        "find-generic-password",
+        "-s",
+        "avante-claude-api",
+        "-a",
+        "ANTHROPIC_API_KEY",
+        "-w",
+      },
     },
   },
 }
