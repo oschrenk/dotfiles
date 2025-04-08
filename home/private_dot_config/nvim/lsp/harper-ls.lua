652e434d9633c48739eb098c90d5c47ce1da5af0
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-04-08 22:01:24 +0200

    nvim, harper: Fix settings

--- home/private_dot_config/nvim/lsp/harper-ls.lua
+++ home/private_dot_config/nvim/lsp/harper-ls.lua
@@ -2,16 +2,18 @@
 -- https://writewithharper.com
 -- brew install harper
 return {
-  cmd = { "harper", "--stdio" },
+  cmd = { "harper-ls", "--stdio" },
   root_markers = { "tsconfig.json" },
   filetypes = {
     "markdown",
     "gitcommit",
   },
   settings = {
-    linters = {
-      SentenceCapitalization = false,
-      SpellCheck = false,
+    ["harper-ls"] = {
+      linters = {
+        SentenceCapitalization = false,
+        SpellCheck = false,
+      },
     },
   },
 }
