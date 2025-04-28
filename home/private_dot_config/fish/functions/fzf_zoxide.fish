224f8641f0fc4a7fa954bae4a002e3aed890ba82
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-04-28 09:23:24 +0200

    fish, fzf_zoxide: Fix

--- home/private_dot_config/fish/functions/fzf_zoxide.fish
+++ home/private_dot_config/fish/functions/fzf_zoxide.fish
@@ -1,3 +1,8 @@
 function fzf_zoxide --description "Open directory"
-    zoxide query --list | fzf
+    zoxide query --list | fzf >"$TMPDIR/fzf.result"
+
+    [ (cat $TMPDIR/fzf.result | wc -l) -gt 0 ]
+    and cd "$(cat "$TMPDIR/fzf.result")"
+    commandline -f repaint
+    rm -f $TMPDIR/fzf.result
 end
