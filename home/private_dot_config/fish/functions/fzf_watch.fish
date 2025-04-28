79274f6a1b7ea59286436d63b3ce40d123c9bbfd
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-04-28 09:23:04 +0200

    fish, fzf_watch: Remove unneeded config

--- home/private_dot_config/fish/functions/fzf_watch.fish
+++ home/private_dot_config/fish/functions/fzf_watch.fish
@@ -1,12 +1,8 @@
 function fzf_watch --description "Watch video"
     set -l base_dir_normal "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Watch"
 
-    set -q FZF_TMUX_HEIGHT; or set FZF_TMUX_HEIGHT 40%
-    set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT --reverse $FZF_DEFAULT_OPTS"
-
     find -L "$base_dir_normal" -type f -maxdepth 4 | cut -d / -f 8- | grep -iE 'mkv|mp4' | fzf >"$TMPDIR/fzf.result"
 
-    # Fish hangs if the command before pipe redirects (2> /dev/null)
     [ (cat $TMPDIR/fzf.result | wc -l) -gt 0 ]
     and open "$base_dir_normal"/(cat $TMPDIR/fzf.result)
     rm -f $TMPDIR/fzf.result
