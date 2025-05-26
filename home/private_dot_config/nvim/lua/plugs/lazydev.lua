14a8697f6229fb02625e3dd91012f1faab3cb3e4
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-05-25 20:12:36 -0600

    nvim: lazydev

---
+++ home/private_dot_config/nvim/lua/plugs/lazydev.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        -- See the configuration section for more details
+      },
+    },
+  },
+  { -- optional blink completion source for require statements and module annotations
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        -- add lazydev to your completion providers
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
+      },
+    },
+  },
+}
