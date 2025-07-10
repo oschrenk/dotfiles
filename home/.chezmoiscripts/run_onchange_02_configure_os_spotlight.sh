6dd77f0ce69505a1c72393969dede510b873145a
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-07-10 10:28:03 +0200

    macos: disable spotlight changes

--- home/.chezmoiscripts/run_onchange_02_configure_os_spotlight.sh
+++ home/.chezmoiscripts/run_onchange_02_configure_os_spotlight.sh
@@ -27,7 +27,11 @@
 # Tips (TIPS)
 # Websites (BOOKMARKS)
 echo "Spotlight: Set defaults"
-defaults write "com.apple.Spotlight" "orderedItems" '({enabled=1;name=APPLICATIONS;},{enabled=1;name="MENU_EXPRESSION";},{enabled=1;name=CONTACT;},{enabled=1;name="MENU_CONVERSION";},{enabled=0;name="MENU_DEFINITION";},{enabled=0;name=SOURCE;},{enabled=0;name=DOCUMENTS;},{enabled=1;name="EVENT_TODO";},{enabled=0;name=DIRECTORIES;},{enabled=0;name=FONTS;},{enabled=0;name=IMAGES;},{enabled=1;name=MESSAGES;},{enabled=0;name=MOVIES;},{enabled=0;name=MUSIC;},{enabled=0;name="MENU_OTHER";},{enabled=0;name=PDF;},{enabled=0;name=PRESENTATIONS;},{enabled=0;name="MENU_SPOTLIGHT_SUGGESTIONS";},{enabled=0;name=SPREADSHEETS;},{enabled=1;name="SYSTEM_PREFS";},{enabled=0;name=TIPS;},{enabled=0;name=BOOKMARKS;},)'
+# the following approach doesn't work anymore, it breaks spotlight settings
+# defaults write "com.apple.Spotlight" "orderedItems" '({enabled=1;name=APPLICATIONS;},{enabled=1;name="MENU_EXPRESSION";},{enabled=1;name=CONTACT;},{enabled=1;name="MENU_CONVERSION";},{enabled=0;name="MENU_DEFINITION";},{enabled=0;name=SOURCE;},{enabled=0;name=DOCUMENTS;},{enabled=1;name="EVENT_TODO";},{enabled=0;name=DIRECTORIES;},{enabled=0;name=FONTS;},{enabled=0;name=IMAGES;},{enabled=1;name=MESSAGES;},{enabled=0;name=MOVIES;},{enabled=0;name=MUSIC;},{enabled=0;name="MENU_OTHER";},{enabled=0;name=PDF;},{enabled=0;name=PRESENTATIONS;},{enabled=0;name="MENU_SPOTLIGHT_SUGGESTIONS";},{enabled=0;name=SPREADSHEETS;},{enabled=1;name="SYSTEM_PREFS";},{enabled=0;name=TIPS;},{enabled=0;name=BOOKMARKS;},)'
+#
+# maybe this works better but needs investigation
+# /usr/libexec/PlistBuddy -c "Delete ':orderedItems:21:enabled'" -c "Add ':orderedItems:21:enabled' bool 'false'" "$HOME/Library/Preferences/com.apple.Spotlight.plist"
 
 ###########################################################
 # Kill affected applications                              #
