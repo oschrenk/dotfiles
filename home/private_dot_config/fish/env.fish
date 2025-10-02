79692d03b57698e588439b1266a8ea281f7b172c
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-10-02 08:25:07 -0600

    fish: remove wakatime

--- home/private_dot_config/fish/env.fish
+++ home/private_dot_config/fish/env.fish
@@ -81,9 +81,6 @@ set -x SWIFTLY_HOME_DIR "$XDG_DATA_HOME/swiftly"
 set -x SWIFTLY_BIN_DIR "$SWIFTLY_HOME_DIR/bin"
 fish_add_path $SWIFTLY_BIN_DIR
 
-# wakatime
-set -x WAKATIME_HOME "$XDG_CONFIG_HOME/wakatime"
-
 # whisper cli via ~/.config/fish/functions/transcribe
 set -x WHISPER_MODEL "$XDG_CACHE_HOME/huggingface/hub/models--distil-whisper--distil-small.en/snapshots/5ced4c93e41c640e72b423d596cc7dc0de3f8419/ggml-distil-small.en.bin"
 
