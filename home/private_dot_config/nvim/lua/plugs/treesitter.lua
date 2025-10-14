d9d2634d744701ebde9583effb1f35f4b12b3d24
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-10-14 17:13:59 -0600

    nvim, treesitter: add kanata

--- home/private_dot_config/nvim/lua/plugs/treesitter.lua
+++ home/private_dot_config/nvim/lua/plugs/treesitter.lua
@@ -33,6 +33,7 @@ return {
         "javascript",
         "jq",
         "json",
+        "kanata",
         "kdl",
         "kotlin",
         "lua",
@@ -118,5 +119,15 @@ return {
         },
       },
     })
+
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    parser_config.kanata = {
+      install_info = {
+        url = "https://github.com/postsolar/tree-sitter-kanata",
+        files = { "src/parser.c" },
+        branch = "master",
+      },
+      filetype = "kbd",
+    }
   end,
 }
