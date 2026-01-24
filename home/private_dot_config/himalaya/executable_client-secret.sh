d4999de09ff3c87348b8927b67428914b9707cc0
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-01-24 12:01:14 -0600

    himalaya: shebang

--- home/private_dot_config/himalaya/executable_client-secret.sh
+++ home/private_dot_config/himalaya/executable_client-secret.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
-# Retrieve client secret from macOS Keychain
+#!/usr/bin/env sh
+
+# retrieve client secret from macOS Keychain
 security find-generic-password -a MELI -s meli-cli -w | jq -r .client_secret
