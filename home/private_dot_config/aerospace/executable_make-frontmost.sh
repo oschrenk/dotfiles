763471fc9243d64bd5d2cb0f605b71fec46b9647
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-01-24 11:50:25 -0600

    aerospace: fix PATH issue

--- home/private_dot_config/aerospace/executable_make-frontmost.sh
+++ home/private_dot_config/aerospace/executable_make-frontmost.sh
@@ -3,8 +3,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+AEROSSPACE_BIN='/opt/homebrew/bin/aerospace'
+
 BUNDLE_ID="${1:-com.apple.finder}"
-ws="$(aerospace list-workspaces --focused)"
+ws="$($AEROSSPACE_BIN list-workspaces --focused)"
 
 # Ensure application has a window and is frontmost
 if [ "$BUNDLE_ID" = "com.apple.finder" ]; then
@@ -28,5 +30,5 @@ fi
 sleep 0.05
 
 # Move the focused Finder window back to the saved workspace and return there
-aerospace move-node-to-workspace "$ws"
-aerospace workspace "$ws"
+$AEROSSPACE_BIN move-node-to-workspace "$ws"
+$AEROSSPACE_BIN workspace "$ws"
