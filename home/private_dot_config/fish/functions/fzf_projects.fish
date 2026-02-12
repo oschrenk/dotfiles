65edb0298f93ea28e42ba5d2fdefdd1bd6671fb3
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-02-11 22:59:36 -0600

    fish, fzf_projects.fish: Use sessionizer

--- home/private_dot_config/fish/functions/fzf_projects.fish
+++ home/private_dot_config/fish/functions/fzf_projects.fish
@@ -1,16 +1,6 @@
-function fzf_projects --description "Browse git projects using fzf"
-    set -l base_dir $HOME/Projects
-    set -l length (math (string length $base_dir) + 2)
-
-    set -q FZF_TMUX_HEIGHT; or set FZF_TMUX_HEIGHT 40%
-    set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT --reverse $FZF_DEFAULT_OPTS"
-
-    set -q FZF_PROJECT_COMMAND; or set -l FZF_PROJECT_COMMAND "
-  find -L $base_dir -type d -name ".git" -maxdepth 4 | rev | cut -c6- | rev | cut -c$length-"
-    # Fish hangs if the command before pipe redirects (2> /dev/null)
-    eval "$FZF_PROJECT_COMMAND | "(__fzfcmd)" +m $FZF_PROJECT_OPTS > $TMPDIR/fzf.result"
-    [ (cat $TMPDIR/fzf.result | wc -l) -gt 0 ]
-    and cd $base_dir/(cat $TMPDIR/fzf.result)
-    commandline -f repaint
-    rm -f $TMPDIR/fzf.result
+function fzf_projects --description "Browse git projects using sessionizer"
+    set -l dir (sessionizer search --print-path)
+    if test -n "$dir"
+        cd "$dir"
+    end
 end
