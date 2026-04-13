945ce4233a29a4a0b1ad6443f8b8ff7b56503531
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-04-13 13:51:17 -0600

    nix: spotlight

---
+++ nix/modules/spotlight.nix
@@ -0,0 +1,7 @@
+{ ... }:
+
+# Spotlight category configuration (com.apple.Spotlight orderedItems) is not
+# supported in nix-darwin and is notoriously fragile on macOS regardless of tool.
+# Configure manually via System Settings > Siri & Spotlight.
+{
+}
