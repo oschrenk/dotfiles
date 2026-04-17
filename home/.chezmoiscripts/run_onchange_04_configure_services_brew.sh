7d4b0d9e4c227c2a3d94ec98badf5b9ccfd074b4
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-04-17 05:03:49 -0600

    brew: run services

--- home/.chezmoiscripts/run_onchange_04_configure_services_brew.sh
+++ home/.chezmoiscripts/run_onchange_04_configure_services_brew.sh
@@ -2,6 +2,6 @@
 
 # manually select services
 # for an overview do `brew services list`
-for SERVICE in "sketchybar" "tmignore-rs"; do
+for SERVICE in "sketchybar" "tmignore-rs" "ollama" "obsidian-headless" "mission" "atuin"; do
   brew services start "$SERVICE"
 done
