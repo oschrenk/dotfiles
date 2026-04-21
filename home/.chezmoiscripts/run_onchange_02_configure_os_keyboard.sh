6b4b25162a57a91a9bef3b38a85e1f03309aa13d
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-04-21 12:56:29 -0600

    chezmoi: merge calendar shortcuts into keyboard script

--- home/.chezmoiscripts/run_onchange_02_configure_os_keyboard.sh
+++ home/.chezmoiscripts/run_onchange_02_configure_os_keyboard.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+source ~/.local/share/chezmoi/home/.chezmoiscripts/run_onchange_03_configure_apps__helper.sh
+
 #######################################
 # KEYBOARD
 #######################################
@@ -12,12 +14,26 @@ echo "Keyboard shortcut: Disable ^→, and ^← to switch spaces"
 plutil -replace AppleSymbolicHotKeys.79.enabled -bool NO ~/Library/Preferences/com.apple.symbolichotkeys.plist
 plutil -replace AppleSymbolicHotKeys.81.enabled -bool NO ~/Library/Preferences/com.apple.symbolichotkeys.plist
 
+#######################################
+# Calendar
+#######################################
+
+echo "Calendar: Allow Calendar to have custom keyboard shortcuts"
+allowCustomKeyboardShortcutsForApp "com.apple.iCal"
+
+echo "Calendar: Replace Toggle Calendar Sidebar with ⌘S"
+defaults write com.apple.iCal NSUserKeyEquivalents "{
+        'Show Calender List' = '${key_cmd}s';
+        'Hide Calender List' = '${key_cmd}s';
+    }"
+
 ###########################################################
 # Kill affected applications                              #
 ###########################################################
+
 for app in "Dock" "Finder" "System Preferences"; do
   echo "Restarting $app"
-	killall "$app" > /dev/null 2>&1
+  killall "$app" > /dev/null 2>&1
 done
 
-exit 0
+askToRestartApps "Calendar"
