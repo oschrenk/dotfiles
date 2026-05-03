ea66643ddf7f84a9ce72ac5ac2020d881b569a55
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-05-03 15:25:20 -0600

    nix: configure hetzner-1 as exit node

--- nix/hosts/hetzner-1.nix
+++ nix/hosts/hetzner-1.nix
@@ -7,4 +7,9 @@
     owner = "root";
     mode = "0600";
   };
+
+  # Exit node. Clients opt in with `tailscale up --exit-node=hetzner-1`.
+  # IPv4 only: host has no IPv6 configured.
+  services.tailscale.useRoutingFeatures = "server";
+  services.tailscale.extraUpFlags = [ "--advertise-exit-node" ];
 }
