4b750d86a1cb09ba151213031948d31724905566
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-06-27 14:00:41 +0200

    fish: note uni dependency in emoji function
    Document that the emoji function needs uni (github.com/arp242/uni, in nixpkgs as `uni`) so it's clear what to install if the go-installed binary is dropped.

--- nix/modules/home/fish/functions/emoji.fish
+++ nix/modules/home/fish/functions/emoji.fish
@@ -1,5 +1,7 @@
-# adaptation of 
+# adaptation of
 # https://github.com/arp242/uni/blob/master/dmenu-uni
+#
+# needs `uni` — github.com/arp242/uni (in nixpkgs as `uni`)
 function emoji --description "Find emoji"
     uni -c e all | fzf | cut -d ' ' -f1 | tr -d \n | string trim | pbcopy
 end
