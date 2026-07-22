7c1e3eab556cb0f88770df225e92bf51d28c132c
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-07-22 15:24:03 -0600

    karabiner: config 8bitdo micro

--- nix/modules/darwin/brew/gui.nix
+++ nix/modules/darwin/brew/gui.nix
@@ -20,7 +20,6 @@
 
   homebrew.casks = [
     "1password" # password manager
-    "8bitdo-ultimate-software" # 8bitdo controller button remapping
     "alcove" # macos, notch helper
     "antinote" # notes with SoulverCore
     "arc" # chromium based browser
