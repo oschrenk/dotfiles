656d7c82c0a74e7dfca16ed5b5e5f04e690e8364
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-08-13 03:36:07 -0600

    brew: anydoc

--- nix/modules/darwin/brew/base.nix
+++ nix/modules/darwin/brew/base.nix
@@ -38,6 +38,7 @@
     "eddmann/tap/whatsapp-cli" # cli, WhatsApp from terminal
     "glow" # cli, render markdown in terminal
     "oschrenk/made/plan" # cli, fetch next event
+    "oschrenk/personal/anydoc" # docs, convert documents to markdown
     "pandoc" # docs, document converter
     "zola" # web, blogging engine
 
