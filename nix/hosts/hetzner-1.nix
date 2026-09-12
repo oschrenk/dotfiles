5b3df53d3c60885de802be78a6a7bc35b1f97ec8
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-09-12 09:52:18 -0600

    secretspec: migrate

--- nix/hosts/hetzner-1.nix
+++ nix/hosts/hetzner-1.nix
@@ -1,9 +1,12 @@
+let
+  secrets = (import ../secrets.nix).read ../../secretspec.toml;
+in
 {
   networking.hostName = "hetzner-1";
 
   # Tailscale
   services.onepassword-secrets.secrets.tailscaleAuthKey = {
-    reference = "op://2udkjdngrnb6jlr62cd7iq33de/2imqxgbvx6htswijyuswh72kye/hetzner-1";
+    reference = secrets.ref "TAILSCALE_AUTHKEY_HETZNER_1";
     owner = "root";
     mode = "0600";
   };
